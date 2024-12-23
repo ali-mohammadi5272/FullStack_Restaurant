9d7b120e593eb987625c6f259fa273d6902c0e94
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-12-23 13:05:22 +0330

    feat: use Container Component in main.tsx

--- Client_React/src/main.tsx
+++ Client_React/src/main.tsx
@@ -2,12 +2,15 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
+import Container from "./components/Container/Container.tsx";
 import "./assets/styles/main.scss";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <Container>
+        <App />
+      </Container>
     </BrowserRouter>
   </StrictMode>
 );
