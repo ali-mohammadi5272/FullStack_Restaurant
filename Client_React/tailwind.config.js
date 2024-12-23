15fb310b39120136e8a891b12423d637cef83439
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-12-23 13:06:26 +0330

    style: add color to tailwind

--- Client_React/tailwind.config.js
+++ Client_React/tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#FF8A00",
+        secondary: "#3FA72F",
+      },
+    },
   },
   plugins: [],
 };
