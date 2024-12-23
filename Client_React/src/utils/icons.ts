ddc5b9e7b9b1fccf4db399b7705316104968fd4f
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-12-23 11:41:15 +0330

    feat: add some Icons

--- Client_React/src/utils/icons.ts
+++ Client_React/src/utils/icons.ts
@@ -1,5 +1,18 @@
 import { IconDefinition } from "@fortawesome/fontawesome-svg-core";
+import {
+  faAt,
+  faMagnifyingGlass,
+  faLock,
+  faUnlockKeyhole,
+  faHashtag,
+} from "@fortawesome/free-solid-svg-icons";
 
-const icons: Record<string, IconDefinition> = {};
+const icons: Record<string, IconDefinition> = {
+  faAt,
+  faMagnifyingGlass,
+  faLock,
+  faUnlockKeyhole,
+  faHashtag,
+};
 
 export { icons };
