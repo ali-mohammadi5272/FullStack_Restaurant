02ec7ed600e92df5b132a659e87bf0e3bd935eee
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-12-26 16:28:53 +0330

    refactor: remove Container

--- Client_React/src/main.tsx
+++ Client_React/src/main.tsx
@@ -2,15 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
-import Container from "./components/Container/Container.tsx";
 import "./assets/styles/main.scss";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <Container>
-        <App />
-      </Container>
+      <App />
     </BrowserRouter>
   </StrictMode>
 );
