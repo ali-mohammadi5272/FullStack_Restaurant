bbc935b6a297353f8f17d9df31e30064ff3cdc7f
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-12-26 13:08:12 +0330

    refactor: change icon type

--- Client_React/src/components/FontAwesomeIcon/fontAwesomeIcon.types.ts
+++ Client_React/src/components/FontAwesomeIcon/fontAwesomeIcon.types.ts
@@ -1,6 +1,14 @@
+type icon =
+  | "faAt"
+  | "faMagnifyingGlass"
+  | "faLock"
+  | "faUnlockKeyhole"
+  | "faHashtag"
+  | "faCartShopping";
+
 interface FontAwesomeIconPropsType {
   className?: string;
-  icon: string;
+  icon: icon;
 }
 
 export type { FontAwesomeIconPropsType };
