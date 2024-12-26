19dc0f863a80f6aebbca536e533138e2b033b0a2
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-12-26 16:29:48 +0330

    refactor: use Container

--- Client_React/src/layouts/Default/Default.tsx
+++ Client_React/src/layouts/Default/Default.tsx
@@ -1,10 +1,15 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../../components/Navbar/Navbar";
+import Container from "../../components/Container/Container";
 
 const DefaultLayout = (): React.ReactNode => {
   return (
-    <div>
-      <Outlet />
-    </div>
+    <>
+      <Navbar />
+      <Container>
+        <Outlet />
+      </Container>
+    </>
   );
 };
 
