38da5c7975cc126814de7ea0cff072e2065c315f
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-12-28 12:47:58 +0330

    feat: init ant-design

--- Client_Vue/src/main.ts
+++ Client_Vue/src/main.ts
@@ -1,6 +1,10 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import { router } from "./routes";
+import Antd from "ant-design-vue";
 import "./assets/styles/main.scss";
+import "ant-design-vue/dist/reset.css";
 
-createApp(App).use(router).mount("#app");
+const app = createApp(App);
+
+app.use(router).use(Antd).mount("#app");
