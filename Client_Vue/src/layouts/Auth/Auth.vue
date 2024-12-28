6ce730b6b8c432c083c4de1e180667e47e71b9ae
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-12-28 12:46:27 +0330

    feat: create AuthLayout

---
+++ Client_Vue/src/layouts/Auth/Auth.vue
@@ -0,0 +1,11 @@
+<template>
+  <Container>
+    <router-view></router-view>
+  </Container>
+</template>
+
+<script lang="ts" setup>
+import Container from "../../components/Container/Container.vue";
+</script>
+
+<style lang="scss" scoped></style>
