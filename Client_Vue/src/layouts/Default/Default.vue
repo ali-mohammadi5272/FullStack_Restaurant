0fa7b0831a16f08e8bee79b8b1c8f722ff8264a5
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2024-12-29 11:18:59 +0330

    feat: create DefaultLayout Component

---
+++ Client_Vue/src/layouts/Default/Default.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <Navbar />
+    <Container>
+      <slot></slot>
+    </Container>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import Container from "../../components/Container/Container.vue";
+import Navbar from "../../components/Navbar/Navbar.vue";
+</script>
+
+<style lang="scss" scoped></style>
