089b8d6c8c17b93cc14bd3e1d86dd4f8dc7ac0af
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2025-01-02 19:53:39 +0330

    feat: add WelcomeToDelizioso Section to Home

--- Client_React/src/pages/Home/index.tsx
+++ Client_React/src/pages/Home/index.tsx
@@ -1,9 +1,11 @@
 import ItalianCuisine from "./ItalianCuisine";
+import WelcomeToDelizioso from "./WelcomeToDelizioso";
 
 const HomePage = (): React.ReactNode => {
   return (
     <main>
       <ItalianCuisine />
+      <WelcomeToDelizioso />
     </main>
   );
 };
