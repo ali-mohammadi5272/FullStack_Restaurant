d0cd63c68d6abb8dc335ce7ad131cd5b52b4e075
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2025-01-03 21:09:52 +0330

    refactor: change h1 to h2

--- Client_React/src/pages/Home/ItalianCuisine.tsx
+++ Client_React/src/pages/Home/ItalianCuisine.tsx
@@ -5,10 +5,10 @@ const ItalianCuisine = (): React.ReactNode => {
   return (
     <div className="flex justify-between items-center mt-10 sm:mt-20 flex-col md:flex-row gap-10">
       <section className="w-full md:w-1/2">
-        <h1 className="font-bold text-5xl sm:text-6xl">
+        <h2 className="font-bold text-5xl sm:text-6xl">
           <span className="block">Italian</span>
           <span className="block">Cuisine</span>
-        </h1>
+        </h2>
         <p className="text-lg text-[#5C4529] mt-10 mb-20 text-justify">
           Lorem ipsum dolor sit amet consectetur adipisicing elit.
           Exercitationem quibusdam voluptate alias est? Sunt eaque aspernatur
