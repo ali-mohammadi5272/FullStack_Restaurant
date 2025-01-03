4b5358e30d92ffffdfebbd0f681d4a24a346138f
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2025-01-03 21:08:53 +0330

    feat: add Categories

--- Client_React/src/pages/Home/OurPopularMenu.tsx
+++ Client_React/src/pages/Home/OurPopularMenu.tsx
@@ -1,9 +1,12 @@
+import Categories from "../../components/Categories/Categories";
+
 const OurPopularMenu = () => {
   return (
     <div className="mt-10 sm:mt-20">
       <h2 className="block font-bold text-5xl sm:text-6xl text-center">
         Our popular menu
       </h2>
+      <Categories />
     </div>
   );
 };
