6db36ffd1688201c5552a4be0947967c17716cd2
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2025-01-03 21:08:02 +0330

    feat: create OurPopularMenu section

---
+++ Client_React/src/pages/Home/OurPopularMenu.tsx
@@ -0,0 +1,11 @@
+const OurPopularMenu = () => {
+  return (
+    <div className="mt-10 sm:mt-20">
+      <h2 className="block font-bold text-5xl sm:text-6xl text-center">
+        Our popular menu
+      </h2>
+    </div>
+  );
+};
+
+export default OurPopularMenu;
