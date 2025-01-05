d780ff49a54c1057e6576ded69ca3b6e2a92094d
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2025-01-05 20:24:02 +0330

    feat: add Cards Component to OurPopularMenu section

--- Client_React/src/pages/Home/OurPopularMenu.tsx
+++ Client_React/src/pages/Home/OurPopularMenu.tsx
@@ -1,12 +1,14 @@
+import Cards from "../../components/Cards/Cards";
 import Categories from "../../components/Categories/Categories";
 
-const OurPopularMenu = () => {
+const OurPopularMenu = (): React.ReactNode => {
   return (
     <div className="mt-10 sm:mt-20">
       <h2 className="block font-bold text-5xl sm:text-6xl text-center">
         Our popular menu
       </h2>
       <Categories />
+      <Cards />
     </div>
   );
 };
