a1f9b68cbf79d0c0a47f2773e21a57b077d9fc17
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2025-01-19 19:26:19 +0330

    feat: add Footer Component to default layout

--- Client_React/src/layouts/Default/Default.tsx
+++ Client_React/src/layouts/Default/Default.tsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../../components/Navbar/Navbar";
 import Container from "../../components/Container/Container";
+import Footer from "../../components/Footer/Footer";
 
 const DefaultLayout = (): React.ReactNode => {
   return (
@@ -9,6 +10,7 @@ const DefaultLayout = (): React.ReactNode => {
       <Container>
         <Outlet />
       </Container>
+      <Footer />
     </>
   );
 };
