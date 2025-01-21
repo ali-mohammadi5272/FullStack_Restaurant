32bd1ec2d003fed687303758f7eb7f34dac41d66
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2025-01-21 18:56:19 +0330

    feat: create OurGreatestChef Section Component

---
+++ Client_React/src/pages/Home/OurGreatestChef.tsx
@@ -0,0 +1,19 @@
+import CustomButton from "../../components/CustomButton/CustomButton";
+import ChefCards from "../../components/ChefCards/ChefCards.tsx";
+import React from "react";
+
+const OurGreatestChef = (): React.ReactNode => {
+  return (
+    <div className="mt-10 sm:mt-20">
+      <h2 className="font-bold text-5xl sm:text-6xl text-center">
+        Our greatest chef
+      </h2>
+      <ChefCards />
+      <div className="text-center">
+        <CustomButton title="View all" className="bg-primary w-[235px]" />
+      </div>
+    </div>
+  );
+};
+
+export default OurGreatestChef;
