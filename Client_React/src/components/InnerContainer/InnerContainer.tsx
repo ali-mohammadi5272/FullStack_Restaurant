5e031bf702bbab72b70ea7cae7d3cff72581cddb
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2025-01-21 15:44:53 +0330

    Revert "feat: add InnerContainer Component to ReserveATable section"
    This reverts commit c915b31690ce290c85996a68c33affcec8a74c6c.

--- Client_React/src/components/InnerContainer/InnerContainer.tsx
+++ Client_React/src/components/InnerContainer/InnerContainer.tsx
@@ -4,7 +4,7 @@ import { InnerContainerPropsType } from "./innerContainer.type";
 const InnerContainer: React.FC<PropsWithChildren<InnerContainerPropsType>> = ({
   children,
 }) => {
-  return <div className="px-5">{children}</div>;
+  return <div>{children}</div>;
 };
 
 export default InnerContainer;
