d6b5fe748c9980785acf765a940a7f6138b92e2f
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2025-01-21 15:42:55 +0330

    feat: add InnerContainer Component to OurPopularMenu section

--- Client_React/src/pages/Home/OurPopularMenu.tsx
+++ Client_React/src/pages/Home/OurPopularMenu.tsx
@@ -1,14 +1,17 @@
 import MenuCards from "../../components/MenuCards/MenuCards";
 import Categories from "../../components/Categories/Categories";
+import InnerContainer from "../../components/InnerContainer/InnerContainer";
 
 const OurPopularMenu = (): React.ReactNode => {
   return (
     <div className="mt-10 sm:mt-20">
-      <h2 className="block font-bold text-5xl sm:text-6xl text-center">
-        Our popular menu
-      </h2>
-      <Categories />
-      <MenuCards />
+      <InnerContainer>
+        <h2 className="block font-bold text-5xl sm:text-6xl text-center">
+          Our popular menu
+        </h2>
+        <Categories />
+        <MenuCards />
+      </InnerContainer>
     </div>
   );
 };
