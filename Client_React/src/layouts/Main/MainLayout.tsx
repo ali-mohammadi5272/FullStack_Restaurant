a334f1ad5644eea52754857bcfb6ed1defcf4a30
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2025-01-21 15:28:47 +0330

    refactor: remove OuterContainer Component from MainLayout

--- Client_React/src/layouts/Main/MainLayout.tsx
+++ Client_React/src/layouts/Main/MainLayout.tsx
@@ -1,15 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../../components/Navbar/Navbar";
 import Footer from "../../components/Footer/Footer";
-import OuterContainer from "../../components/OuterContainer/OuterContainer";
 
 const MainLayout = (): React.ReactNode => {
   return (
     <>
       <Navbar />
-      <OuterContainer>
-        <Outlet />
-      </OuterContainer>
+      <Outlet />
       <Footer />
     </>
   );
