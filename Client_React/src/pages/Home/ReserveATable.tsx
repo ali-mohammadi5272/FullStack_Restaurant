45e857a261e074664a2d02b8532c35ba7975c538
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2025-01-21 15:43:17 +0330

    feat: add InnerContainer Component to ReserveATable section

--- Client_React/src/pages/Home/ReserveATable.tsx
+++ Client_React/src/pages/Home/ReserveATable.tsx
@@ -1,29 +1,35 @@
 import CustomButton from "../../components/CustomButton/CustomButton";
+import InnerContainer from "../../components/InnerContainer/InnerContainer";
 import foodImage from "./../../assets/images/Let's reserve a table.png";
 
 const ReserveATable = (): React.ReactNode => {
   return (
-    <div className="flex justify-between items-center mt-10 sm:mt-20 flex-col-reverse md:flex-row gap-10 bg-[#FFF4E7] py-10">
-      <section className="w-full md:w-1/2 relative">
-        <img className="w-full h-auto" src={foodImage} alt="Food's Image" />
-      </section>
-      <section className="w-full md:w-1/2">
-        <h2 className="font-bold text-5xl sm:text-6xl">
-          <span className="block">Let's reserve </span>
-          <span className="block text-primary">a table</span>
-        </h2>
-        <p className="text-lg text-[#5C4529] mt-10 mb-20 text-justify">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          Exercitationem quibusdam voluptate alias est? Sunt eaque aspernatur
-          molestias dolor itaque beatae suscipit consequuntur, autem repellat.
-        </p>
-        <div className="flex gap-3 sm:gap-6">
-          <CustomButton
-            title="Reservation"
-            className="w-1/2 bg-primary py-7 font-semibold"
-          />
+    <div className="mt-10 sm:mt-20 bg-[#FFF4E7] py-10">
+      <InnerContainer>
+        <div className="flex justify-between items-center flex-col-reverse md:flex-row gap-10">
+          <section className="w-full md:w-1/2 relative">
+            <img className="w-full h-auto" src={foodImage} alt="Food's Image" />
+          </section>
+          <section className="w-full md:w-1/2">
+            <h2 className="font-bold text-5xl sm:text-6xl">
+              <span className="block">Let's reserve </span>
+              <span className="block text-primary">a table</span>
+            </h2>
+            <p className="text-lg text-[#5C4529] mt-10 mb-20 text-justify">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Exercitationem quibusdam voluptate alias est? Sunt eaque
+              aspernatur molestias dolor itaque beatae suscipit consequuntur,
+              autem repellat.
+            </p>
+            <div className="flex gap-3 sm:gap-6">
+              <CustomButton
+                title="Reservation"
+                className="w-1/2 bg-primary py-7 font-semibold"
+              />
+            </div>
+          </section>
         </div>
-      </section>
+      </InnerContainer>
     </div>
   );
 };
