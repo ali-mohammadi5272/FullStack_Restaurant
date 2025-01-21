c915b31690ce290c85996a68c33affcec8a74c6c
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2025-01-21 15:43:32 +0330

    feat: add InnerContainer Component to ReserveATable section

--- Client_React/src/components/InnerContainer/InnerContainer.tsx
+++ Client_React/src/components/InnerContainer/InnerContainer.tsx
@@ -4,7 +4,7 @@ import { InnerContainerPropsType } from "./innerContainer.type";
 const InnerContainer: React.FC<PropsWithChildren<InnerContainerPropsType>> = ({
   children,
 }) => {
-  return <div>{children}</div>;
+  return <div className="px-5">{children}</div>;
 };
 
 export default InnerContainer;
