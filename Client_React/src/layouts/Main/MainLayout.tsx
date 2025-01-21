62fcae29aaa0eaa058e604320bd82341f688679d
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2025-01-21 15:09:42 +0330

    refactor: use OuterContainer instead Container

--- Client_React/src/layouts/Main/MainLayout.tsx
+++ Client_React/src/layouts/Main/MainLayout.tsx
@@ -1,15 +1,15 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../../components/Navbar/Navbar";
-import Container from "../../components/Container/Container";
 import Footer from "../../components/Footer/Footer";
+import OuterContainer from "../../components/OuterContainer/OuterContainer";
 
 const MainLayout = (): React.ReactNode => {
   return (
     <>
       <Navbar />
-      <Container>
+      <OuterContainer>
         <Outlet />
-      </Container>
+      </OuterContainer>
       <Footer />
     </>
   );
