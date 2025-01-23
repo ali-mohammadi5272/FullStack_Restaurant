c0db6ac210cedba00a75f6d9972cbd399103c1e5
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2025-01-23 16:46:28 +0330

    style: change styles for responsive

--- Client_React/src/pages/Home/WeAreOpenFrom.tsx
+++ Client_React/src/pages/Home/WeAreOpenFrom.tsx
@@ -5,16 +5,16 @@ import React from "react";
 const WeAreOpenFrom = (): React.ReactNode => {
   return (
     <InnerContainer>
-      <div className="my-20 sm:my-40 bg-[url('./../../assets/images/unsplash_gKN5m528N6o.png')] rounded-[80px] py-20">
+      <div className="my-20 sm:my-40 bg-[url('./../../assets/images/unsplash_gKN5m528N6o.png')] bg-no-repeat bg-cover rounded-[80px] py-20">
         <div className="flex justify-center items-center flex-col-reverse md:flex-row gap-10 text-center">
-          <div className="w-full md:w-1/2">
-            <h2 className="font-bold text-5xl sm:text-6xl text-white">
+          <div className="w-full sm:w-2/3 md:w-1/2">
+            <h2 className="font-bold text-3xl sm:text-4xl lg:text-5xl xl:text-6xl text-white">
               we are open from
             </h2>
-            <h3 className="font-bold text-3xl sm:text-4xl text-white my-10">
+            <h3 className="font-bold text-lg sm:text-2xl lg:text-3xl xl:text-4xl text-white my-5 sm:my-7 md:my-10">
               Monday-Sunday
             </h3>
-            <div className="text-lg text-[#5C4529] mb-20 text-white space-y-1">
+            <div className="text-xs sm:text-sm md:text-base lg:text-lg text-[#5C4529] mb-10 mb-20 text-white space-y-1">
               <div className="flex justify-center gap-2">
                 <p>Launch: Mon-Sun :</p>
                 <p>11:00am-02:00pm</p>
@@ -27,14 +27,14 @@ const WeAreOpenFrom = (): React.ReactNode => {
                 </div>
               </div>
             </div>
-            <div className="flex gap-3 sm:gap-6">
+            <div className="flex flex-col sm:flex-row gap-3 sm:gap-6 items-center sm:items-stretch">
               <CustomButton
                 title="Order now"
-                className="w-1/2 bg-primary py-7 font-semibold"
+                className="w-1/2 bg-primary py-7 font-semibold text-sm sm:text-base md:text-lg lg:text-xl"
               />
               <CustomButton
                 title="Reservation"
-                className="w-1/2 bg-[#FFFFFFF2] text-black py-7 font-semibold"
+                className="w-1/2 bg-[#FFFFFFF2] text-black py-7 font-semibold text-sm sm:text-base md:text-lg lg:text-xl"
               />
             </div>
           </div>
