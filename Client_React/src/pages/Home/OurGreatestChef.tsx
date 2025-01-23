8dbf44f47d204e9cd7cb02221039c3bdd5b92d83
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2025-01-23 15:41:18 +0330

    feat: use InnerContainer component

--- Client_React/src/pages/Home/OurGreatestChef.tsx
+++ Client_React/src/pages/Home/OurGreatestChef.tsx
@@ -1,17 +1,20 @@
 import CustomButton from "../../components/CustomButton/CustomButton";
 import ChefCards from "../../components/ChefCards/ChefCards.tsx";
+import InnerContainer from "../../components/InnerContainer/InnerContainer.tsx";
 import React from "react";
 
 const OurGreatestChef = (): React.ReactNode => {
   return (
     <div className="my-20 sm:my-40">
-      <h2 className="font-bold text-5xl sm:text-6xl text-center">
-        Our greatest chef
-      </h2>
-      <ChefCards />
-      <div className="text-center">
-        <CustomButton title="View all" className="bg-primary w-[235px]" />
-      </div>
+      <InnerContainer>
+        <h2 className="font-bold text-5xl sm:text-6xl text-center">
+          Our greatest chef
+        </h2>
+        <ChefCards />
+        <div className="text-center">
+          <CustomButton title="View all" className="bg-primary w-[235px]" />
+        </div>
+      </InnerContainer>
     </div>
   );
 };
