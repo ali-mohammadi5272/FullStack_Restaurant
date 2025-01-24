5e35b5dd7ad62d71cc3390ed499c2230ac7d8e5a
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2025-01-24 19:24:58 +0330

    feat: add WelcomeToDelizioso Section to Home

--- Client_Vue/src/pages/Home/index.vue
+++ Client_Vue/src/pages/Home/index.vue
@@ -1,11 +1,13 @@
 <template>
   <main>
     <ItalianCuisine />
+    <WelcomeToDelizioso />
   </main>
 </template>
 
 <script lang="ts" setup>
 import ItalianCuisine from "./ItalianCuisine.vue";
+import WelcomeToDelizioso from "./WelcomeToDelizioso.vue";
 </script>
 
 <style lang="scss" scoped></style>
