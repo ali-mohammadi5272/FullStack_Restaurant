04daca6581cbe5d032107076ce8c913251f4619b
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2025-01-24 19:13:04 +0330

    feat: add ItalianCuisine Section to Home

--- Client_Vue/src/pages/Home/index.vue
+++ Client_Vue/src/pages/Home/index.vue
@@ -1,7 +1,11 @@
 <template>
-  <main></main>
+  <main>
+    <ItalianCuisine />
+  </main>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import ItalianCuisine from "./ItalianCuisine.vue";
+</script>
 
 <style lang="scss" scoped></style>
