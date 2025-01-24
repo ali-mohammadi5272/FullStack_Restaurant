39e5209077ffb2edb04898b81cc232775a1042b4
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2025-01-24 19:23:42 +0330

    feat: create WelcomeToDelizioso Section Component

---
+++ Client_Vue/src/pages/Home/WelcomeToDelizioso.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="my-20 sm:my-40 bg-[#ecfaf1] py-24 md:py-12">
+    <InnerContainer>
+      <div
+        class="flex justify-between items-center flex-col-reverse md:flex-row gap-10"
+      >
+        <section class="w-full md:w-1/2">
+          <img :src="foodImage" alt="Food's Image" class="w-full h-auto" />
+        </section>
+        <section class="w-full md:w-1/2">
+          <h2 class="font-bold text-5xl sm:text-6xl">
+            <span class="block">Welcome to</span>
+            <span class="block text-primary">delizioso</span>
+          </h2>
+          <p class="text-lg text-[#5C4529] mt-10 mb-20 text-justify">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Exercitationem quibusdam voluptate alias est? Sunt eaque aspernatur
+            molestias dolor itaque beatae suscipit consequuntur, autem repellat.
+          </p>
+          <div class="flex gap-3 sm:gap-6">
+            <CustomButton
+              class="w-1/2 bg-primary py-7 font-semibold"
+              title="See our menu"
+            />
+          </div>
+        </section>
+      </div>
+    </InnerContainer>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import CustomButton from "../../components/CustomButton/CustomButton.vue";
+import InnerContainer from "../../components/InnerContainer/InnerContainer.vue";
+import foodImage from "./../../assets/images/Picture (3).png";
+</script>
+
+<style lang="scss" scoped></style>
