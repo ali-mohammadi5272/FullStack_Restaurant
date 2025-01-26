8c9330bece5389626bcb0ec65d5bf2e1ae64cb45
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2025-01-26 16:34:58 +0330

    refactor: remove useless import

--- Client_Vue/src/components/CustomButton/CustomButton.vue
+++ Client_Vue/src/components/CustomButton/CustomButton.vue
@@ -1,15 +1,14 @@
 <template>
-  <Button
+  <a-button
     :class="`base-button rounded-[162.94px] ${props.class}`"
     :htmlType="props.htmlType ? props.htmlType : 'button'"
     :type="props.type ? props.type : 'primary'"
   >
     {{ props.title }}
-  </Button>
+  </a-button>
 </template>
 
 <script lang="ts" setup>
-import { Button } from "ant-design-vue";
 import type { ButtonPropsType } from "./customButton.types";
 
 const props = defineProps<ButtonPropsType>();
