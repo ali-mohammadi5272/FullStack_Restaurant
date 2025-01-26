7198349d836a999908b1f174f0747d4cf20b9ce6
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2025-01-26 17:19:03 +0330

    feat: create MenuCard Component

---
+++ Client_Vue/src/components/MenuCard/MenuCard.vue
@@ -0,0 +1,38 @@
+<template>
+  <article class="bg-gray-50 rounded-[70px] overflow-hidden">
+    <section>
+      <img :src="props.src" alt="Food's Image" class="w-full h-auto" />
+    </section>
+    <section class="px-7 pb-10 text-center">
+      <header>
+        <h1 class="text-3xl font-bold">{{ props.title }}</h1>
+      </header>
+      <div class="my-5">
+        <a-rate :defaultValue="props.rate" class="text-primary" disabled />
+      </div>
+      <p class="mb-8 leading-7">{{ props.description }}</p>
+      <footer class="flex items-center">
+        <strong class="flex-grow w-1/2 text-2xl">${{ props.price }}</strong>
+        <CustomButton
+          class="flex-grow w-1/2 font-bold bg-primary hidden sm:inline-block cursor-pointer"
+          title="Order bow"
+        />
+        <CustomButton
+          class="sm:flex-grow w-[50px] sm:w-1/2 font-bold rounded-full bg-primary inline-block sm:hidden cursor-pointer"
+        >
+          <FontAwesomeIcon icon="plus" />
+        </CustomButton>
+      </footer>
+    </section>
+  </article>
+</template>
+
+<script lang="ts" setup>
+import CustomButton from "../CustomButton/CustomButton.vue";
+import FontAwesomeIcon from "../FontAwesomeIcon/FontAwesomeIcon.vue";
+import type { MenuCardPropsType } from "./menuCard.types.ts";
+
+const props = defineProps<MenuCardPropsType>();
+</script>
+
+<style lang="scss" scoped></style>
---
+++ Client_Vue/src/components/MenuCard/menuCard.types.ts
@@ -0,0 +1,9 @@
+interface MenuCardPropsType {
+  title: string;
+  description: string;
+  src: string;
+  price: number;
+  rate: 1 | 2 | 3 | 4 | 5;
+}
+
+export type { MenuCardPropsType };
