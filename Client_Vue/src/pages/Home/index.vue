702f78c7e7dcac49b103c52f3ee46faa4883252a
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2025-01-27 15:45:56 +0330

    feat: add OurPopularMenu Section to Home

--- Client_Vue/src/pages/Home/index.vue
+++ Client_Vue/src/pages/Home/index.vue
@@ -2,12 +2,14 @@
   <main>
     <ItalianCuisine />
     <WelcomeToDelizioso />
+    <OurPopularMenu />
   </main>
 </template>
 
 <script lang="ts" setup>
 import ItalianCuisine from "./ItalianCuisine.vue";
 import WelcomeToDelizioso from "./WelcomeToDelizioso.vue";
+import OurPopularMenu from "./OurPopularMenu.vue";
 </script>
 
 <style lang="scss" scoped></style>
