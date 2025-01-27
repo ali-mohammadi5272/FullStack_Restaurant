36f7937f1a5b654dfd6d151759676152d7b486ad
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2025-01-27 15:44:55 +0330

    feat: create OurPopularMenu Section Component

---
+++ Client_Vue/src/pages/Home/OurPopularMenu.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="my-20 sm:my-40">
+    <InnerContainer>
+      <h2 class="block font-bold text-5xl sm:text-6xl text-center">
+        Our popular menu
+      </h2>
+      <Categories />
+      <MenuCards />
+    </InnerContainer>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import InnerContainer from "../../components/InnerContainer/InnerContainer.vue";
+import Categories from "../../components/Categories/Categories.vue";
+import MenuCards from "../../components/MenuCards/MenuCards.vue";
+</script>
+
+<style lang="scss" scoped></style>
