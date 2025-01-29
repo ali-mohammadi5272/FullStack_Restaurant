b9aa1ef6bf4f03f211b764d4234bb6381a0cd356
ali-mohammadi5272 <ali.mohammadi5272@gmail.com>
2025-01-29 12:16:30 +0330

    feat: add Footer to MainLayout

--- Client_Vue/src/layouts/Main/MainLayout.vue
+++ Client_Vue/src/layouts/Main/MainLayout.vue
@@ -2,11 +2,13 @@
   <div>
     <Navbar />
     <router-view></router-view>
+    <Footer />
   </div>
 </template>
 
 <script lang="ts" setup>
 import Navbar from "../../components/Navbar/Navbar.vue";
+import Footer from "../../components/Footer/Footer.vue";
 </script>
 
 <style lang="scss" scoped></style>
