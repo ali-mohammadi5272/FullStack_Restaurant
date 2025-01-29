7e17a71dfc9f54e3f13aba2691ec3df29e1095eb
ali-mohammadi5272 <ali.mohammadi5272@gmail.com>
2025-01-29 18:44:22 +0330

    feat: create ChefCard Component

---
+++ Client_Vue/src/components/ChefCard/ChefCard.vue
@@ -0,0 +1,21 @@
+<template>
+  <article class="text-center text-2xl space-y-4 sm:space-y-8">
+    <img
+        :class="`w-full h-auto rounded-[50px] ${props.imageClass ? props.imageClass : ''}`"
+        :src="props.img"
+        alt="Food's Image"
+    />
+    <h3 class="text-sm sm:text-lg md:text-2xl font-semibold text-[#311F09]">
+      {{ props.name }}
+    </h3>
+    <h4 class="text-sm sm:text-lg md:text-2xl text-[#A08D76]">{{ props.role }}</h4>
+  </article>
+</template>
+
+<script lang="ts" setup>
+import type {ChefCardPropsType} from "./chefCard.type";
+
+const props = defineProps<ChefCardPropsType>()
+</script>
+
+<style lang="scss" scoped></style>
---
+++ Client_Vue/src/components/ChefCard/chefCard.type.ts
@@ -0,0 +1,8 @@
+interface ChefCardPropsType {
+    name: string;
+    role: string;
+    img: string;
+    imageClass?: string;
+}
+
+export type {ChefCardPropsType};
