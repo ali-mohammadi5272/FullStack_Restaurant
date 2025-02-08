ae627654d3d28aa01316cbb5a889b5ad7dbece70
ali-mohammadi5272 <ali.mohammadi5272@gmail.com>
2025-02-08 16:44:57 +0330

    refactor: change MenuPage Route

--- Client_Vue/src/components/Navbar/Navbar.vue
+++ Client_Vue/src/components/Navbar/Navbar.vue
@@ -33,7 +33,7 @@ import InnerContainer from "../InnerContainer/InnerContainer.vue";
 
 const links: NavbarLinkType[] = [
   { id: 1, title: "Home", to: "/" },
-  { id: 2, title: "Menu", to: "/menu" },
+  { id: 2, title: "Menu", to: "/menus" },
   { id: 3, title: "About us", to: "/about" },
   { id: 4, title: "Order Online", to: "/order-online" },
   { id: 5, title: "Reservation", to: "/reservation" },
