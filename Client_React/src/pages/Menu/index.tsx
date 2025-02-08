22aaa50c85f6f0eaf6676837db28277c45208e0b
ali-mohammadi5272 <ali.mohammadi5272@gmail.com>
2025-02-08 16:34:55 +0330

    feat: complete MenuPage

--- Client_React/src/pages/Menu/index.tsx
+++ Client_React/src/pages/Menu/index.tsx
@@ -1,7 +1,20 @@
 import React from "react";
+import Categories from "../../components/Categories/Categories.tsx";
+import MenuCards from "../../components/MenuCards/MenuCards.tsx";
+import InnerContainer from "../../components/InnerContainer/InnerContainer.tsx";
 
 const MenuPage = (): React.ReactNode => {
-  return <main></main>;
+  return (
+    <main className="my-20 sm:my-40">
+      <InnerContainer>
+        <h2 className="block font-bold text-3xl sm:text-[45px] md:text-[60px] lg:text-[80px] text-center sm:mb-32">
+          Menu
+        </h2>
+        <Categories />
+        <MenuCards />
+      </InnerContainer>
+    </main>
+  );
 };
 
 export default MenuPage;
