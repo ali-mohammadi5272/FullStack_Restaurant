6e7d374af10b9140d0238dfb974b5554ca0a4c76
ali-mohammadi5272 <ali.mohammadi5272@gmail.com>
2025-02-09 13:37:04 +0330

    feat: add OwnerAndExecutiveChef Section to About_Us Page

--- Client_Vue/src/pages/About_Us/index.vue
+++ Client_Vue/src/pages/About_Us/index.vue
@@ -1,11 +1,13 @@
 <template>
   <main>
     <OurRestaurant />
+    <OwnerAndExecutiveChef />
   </main>
 </template>
 
 <script lang="ts" setup>
 import OurRestaurant from "./OurRestaurant.vue";
+import OwnerAndExecutiveChef from "./OwnerAndExecutiveChef.vue";
 </script>
 
 <style lang="scss" scoped></style>
