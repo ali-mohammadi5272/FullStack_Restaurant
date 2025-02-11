1656de9bacab06a5c8a76795273a181858f89631
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2025-02-11 14:29:00 +0330

    feat: add OwnerAndExecutiveChef Section to AboutUs Page

--- Client_React/src/pages/About_Us/index.tsx
+++ Client_React/src/pages/About_Us/index.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import OurRestaurant from "./OurRestaurant.tsx";
+import OwnerAndExecutiveChef from "./OwnerAndExecutiveChef.tsx";
 
 const AboutUsPage = (): React.ReactNode => {
   return (
     <main>
       <OurRestaurant />
+      <OwnerAndExecutiveChef />
     </main>
   );
 };
