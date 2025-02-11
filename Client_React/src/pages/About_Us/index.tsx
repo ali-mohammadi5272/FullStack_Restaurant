a4afde0432b31e70afcf31bef7561cdf9378988f
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2025-02-11 14:28:47 +0330

    feat: add OurRestaurant Section to AboutUs Page

--- Client_React/src/pages/About_Us/index.tsx
+++ Client_React/src/pages/About_Us/index.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import OurRestaurant from "./OurRestaurant.tsx";
 
 const AboutUsPage = (): React.ReactNode => {
-  return <main></main>;
+  return (
+    <main>
+      <OurRestaurant />
+    </main>
+  );
 };
 
 export default AboutUsPage;
