881374b130e3b79a83b922e2c7b061fa9a557b43
ali-mohammadi5272 <ali.mohammadi5272@gmail.com>
2025-02-19 19:56:20 +0330

    feat: add new props to InnerContainer component

--- Client_React/src/components/InnerContainer/InnerContainer.tsx
+++ Client_React/src/components/InnerContainer/InnerContainer.tsx
@@ -3,8 +3,23 @@ import { InnerContainerPropsType } from "./innerContainer.type";
 
 const InnerContainer: React.FC<PropsWithChildren<InnerContainerPropsType>> = ({
   children,
+  position,
 }) => {
-  return <div className="px-5 sm:px-10">{children}</div>;
+  return (
+    <div
+      className={
+        position === "right"
+          ? "pr-5 sm:pr-10"
+          : position === "left"
+            ? "pl-5 sm:pl-10"
+            : !position
+              ? "px-5 sm:px-10"
+              : ""
+      }
+    >
+      {children}
+    </div>
+  );
 };
 
 export default InnerContainer;
--- Client_React/src/components/InnerContainer/innerContainer.type.ts
+++ Client_React/src/components/InnerContainer/innerContainer.type.ts
@@ -1,5 +1,8 @@
+import React from "react";
+
 interface InnerContainerPropsType {
   children: React.ReactNode;
+  position?: "right" | "left";
 }
 
 export type { InnerContainerPropsType };
