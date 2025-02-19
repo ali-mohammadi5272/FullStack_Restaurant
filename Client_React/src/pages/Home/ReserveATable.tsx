5d06d2773049a0adf3bbc5c0854426b1a9a967b5
ali-mohammadi5272 <ali.mohammadi5272@gmail.com>
2025-02-19 20:15:24 +0330

    refactor: remove useless element

--- Client_React/src/pages/Home/ReserveATable.tsx
+++ Client_React/src/pages/Home/ReserveATable.tsx
@@ -21,12 +21,10 @@ const ReserveATable = (): React.ReactNode => {
               aspernatur molestias dolor itaque beatae suscipit consequuntur,
               autem repellat.
             </p>
-            <div className="flex gap-3 sm:gap-6">
-              <CustomButton
-                title="Reservation"
-                className="w-1/2 bg-primary py-7 font-semibold"
-              />
-            </div>
+            <CustomButton
+              title="Reservation"
+              className="w-1/2 bg-primary py-7 font-semibold"
+            />
           </section>
         </div>
       </InnerContainer>
