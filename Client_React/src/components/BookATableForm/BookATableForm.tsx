c4fadf66a16bced8735e4aa98591aa3f9ee6adb1
ali-mohammadi5272 <ali.mohammadi5272@gmail.com>
2025-02-19 20:10:43 +0330

    feat: create BookATableForm Component

---
+++ Client_React/src/components/BookATableForm/BookATableForm.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import CustomInput from "../CustomInput/CustomInput.tsx";
+import FormButton from "../FormButton/FormButton.tsx";
+import { Form, InputNumber } from "antd";
+
+const BookATableForm = (): React.ReactNode => {
+  const [form] = Form.useForm();
+
+  return (
+    <Form
+      name="wrap"
+      labelAlign="left"
+      form={form}
+      labelWrap
+      wrapperCol={{ flex: 1 }}
+    >
+      <CustomInput
+        rules={[{ required: true }]}
+        label="Email"
+        type="email"
+        name="Email"
+      />
+      <CustomInput
+        rules={[{ required: true }]}
+        label="Password"
+        type="password"
+        name="Password"
+      />
+
+      <Form.Item
+        rules={[{ required: true }]}
+        label="Party Size"
+        name="Party Size"
+      >
+        <InputNumber
+          min={1}
+          className="base-input w-full flex items-center"
+          type="number"
+        />
+      </Form.Item>
+      <Form.Item>
+        <FormButton
+          htmlType="submit"
+          className="bg-primary w-full"
+          title="Log in"
+        />
+      </Form.Item>
+    </Form>
+  );
+};
+
+export default BookATableForm;
