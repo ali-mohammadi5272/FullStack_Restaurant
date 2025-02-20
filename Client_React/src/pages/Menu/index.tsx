4c5a18665a5d03ee21e4cfc592e7e04e49838223
ali-mohammadi5272 <ali.mohammadi5272@gmail.com>
2025-02-20 09:19:14 +0330

    refactor: use Menu Component

--- Client_React/src/pages/Menu/index.tsx
+++ Client_React/src/pages/Menu/index.tsx
@@ -1,18 +1,10 @@
 import React from "react";
-import Categories from "../../components/Categories/Categories.tsx";
-import MenuCards from "../../components/MenuCards/MenuCards.tsx";
-import InnerContainer from "../../components/InnerContainer/InnerContainer.tsx";
+import Menu from "./Menu";
 
 const MenuPage = (): React.ReactNode => {
   return (
     <main className="my-20 sm:my-40">
-      <InnerContainer>
-        <h2 className="block font-bold text-3xl sm:text-[45px] md:text-[60px] lg:text-[80px] text-center sm:mb-32">
-          Menu
-        </h2>
-        <Categories />
-        <MenuCards />
-      </InnerContainer>
+      <Menu />
     </main>
   );
 };
