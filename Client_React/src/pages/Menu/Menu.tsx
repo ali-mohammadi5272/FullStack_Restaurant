7eaa3d2e757bcdeaec7f40b272508a425d18f8ff
ali-mohammadi5272 <ali.mohammadi5272@gmail.com>
2025-02-20 09:18:59 +0330

    feat: create Menu Section Component

---
+++ Client_React/src/pages/Menu/Menu.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Categories from "../../components/Categories/Categories.tsx";
+import MenuCards from "../../components/MenuCards/MenuCards.tsx";
+import InnerContainer from "../../components/InnerContainer/InnerContainer.tsx";
+
+const Menu = (): React.ReactNode => {
+  return (
+    <InnerContainer>
+      <h2 className="block font-bold text-3xl sm:text-[45px] md:text-[60px] lg:text-[80px] text-center sm:mb-32">
+        Menu
+      </h2>
+      <Categories />
+      <MenuCards />
+    </InnerContainer>
+  );
+};
+
+export default Menu;
