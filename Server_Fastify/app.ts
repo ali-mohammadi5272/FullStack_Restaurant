9136f979833be626720aece1e8031e91ace50835
ali-mohammadi5272 <ali.mohammadi5272@gmail.com>
2025-03-02 15:44:24 +0330

    feat: connect to database

--- Server_Fastify/app.ts
+++ Server_Fastify/app.ts
@@ -1,12 +1,19 @@
 import fastify from "fastify";
 import { env } from "./src/utils/env/env";
+import { sequelize } from "./src/configs/db";
 
 const server = fastify();
 
-server.listen({ port: env.port }, (err, address) => {
+server.listen({ port: env.port }, async (err, address) => {
   if (err) {
     console.error(err);
     process.exit(1);
   }
-  console.log(`Server listening at ${address}`);
+  try {
+    await sequelize.sync();
+    console.log(`Server listening at ${address}`);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
 });
