92140821dda359f87b16dce085a86bd01ae5054b
ali-mohammadi5272 <ali.mohammadi5272@gmail.com>
2025-03-03 17:45:55 +0330

    feat: create login route

--- Server_Fastify/src/modules/auth/auth.route.ts
+++ Server_Fastify/src/modules/auth/auth.route.ts
@@ -1,9 +1,11 @@
 import authController from "./auth.controller";
 import registerSchema from "../../utils/validators/Auth/register";
+import loginSchema from "../../utils/validators/Auth/login";
 import { FastifyInstance } from "fastify";
 import { env } from "../../utils/env/env";
 import { bodyValidator } from "../../utils/middlewares/bodyValidator";
 import { CreateOneDtoType } from "../user/dto/create-one.dto";
+import { LoginDtoType } from "./dto/login.dto";
 
 const router = (server: FastifyInstance) => {
   server.post(
@@ -12,6 +14,14 @@ const router = (server: FastifyInstance) => {
       preHandler: [bodyValidator<CreateOneDtoType>(registerSchema)],
     },
     authController.register
+  );
+  
+  server.post(
+    `${env.baseUrl}/auth/login`,
+    {
+      preHandler: [bodyValidator<LoginDtoType>(loginSchema)],
+    },
+    authController.login
   );
 };
 
