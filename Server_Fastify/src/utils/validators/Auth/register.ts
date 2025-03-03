c4a37a5231bdfa3a8451aa8f21006a2ee725e6f3
ali-mohammadi5272 <ali.mohammadi5272@gmail.com>
2025-03-03 13:54:19 +0330

    refactor: change registerSchema validator

--- Server_Fastify/src/utils/validators/Auth/register.ts
+++ Server_Fastify/src/utils/validators/Auth/register.ts
@@ -1,10 +1,10 @@
-import * as Yup from "yup";
+import { object, string } from "yup";
 import { userNamePattern } from "../../patterns/patterns";
 
-const registerSchema = Yup.object().shape({
-  userName: Yup.string().min(3).required().matches(userNamePattern),
-  email: Yup.string().min(5).email().required(),
-  password: Yup.string().min(8).required(),
+const registerSchema = object().shape({
+  userName: string().min(3).required().matches(userNamePattern),
+  email: string().min(5).email().required(),
+  password: string().min(8).required(),
 });
 
 export default registerSchema;
