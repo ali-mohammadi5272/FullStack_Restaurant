fa312cd992d5af147e653595f2f2581aaeda0522
ali-mohammadi5272 <ali.mohammadi5272@gmail.com>
2025-03-03 13:11:29 +0330

    feat: create createOne service

--- Server_Fastify/src/modules/refreshToken/refreshToken.service.ts
+++ Server_Fastify/src/modules/refreshToken/refreshToken.service.ts
@@ -1,3 +1,10 @@
-const service = {};
+import { CreateOneDtoType } from "./dto/create-one.dto";
+import RefreshToken from "./refreshToken.model";
+
+const service = {
+  async createOne(body: CreateOneDtoType) {
+    return await RefreshToken.create(body);
+  },
+};
 
 export default service;
