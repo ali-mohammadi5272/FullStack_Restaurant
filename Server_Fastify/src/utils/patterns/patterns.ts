49ba65bb955c14f877a05a450780a70e48a80a95
ali-mohammadi5272 <ali.mohammadi5272@gmail.com>
2025-03-03 13:17:18 +0330

    feat: add emailPattern RegEx

--- Server_Fastify/src/utils/patterns/patterns.ts
+++ Server_Fastify/src/utils/patterns/patterns.ts
@@ -1,3 +1,4 @@
 const userNamePattern: RegExp = /^[a-z]{1}[a-z0-9]{2,}(_[a-z0-9]+)*?$/;
+const emailPattern: RegExp = /^[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+$/;
 
-export { userNamePattern };
+export { userNamePattern, emailPattern };
