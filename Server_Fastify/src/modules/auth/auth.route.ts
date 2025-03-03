302b780bac32804f174eab9519d959bf7ef9cb43
ali-mohammadi5272 <ali.mohammadi5272@gmail.com>
2025-03-03 15:40:27 +0330

    feat: create register router

--- Server_Fastify/src/modules/auth/auth.route.ts
+++ Server_Fastify/src/modules/auth/auth.route.ts
@@ -1,3 +1,18 @@
-const route = () => {};
+import authController from "./auth.controller";
+import registerSchema from "../../utils/validators/Auth/register";
+import { FastifyInstance } from "fastify";
+import { env } from "../../utils/env/env";
+import { bodyValidator } from "../../utils/middlewares/bodyValidator";
+import { CreateOneDtoType } from "../user/dto/create-one.dto";
+
+const route = (server: FastifyInstance) => {
+  server.post(
+    `/api/${env.version}/auth/register`,
+    {
+      preHandler: [bodyValidator<CreateOneDtoType>(registerSchema)],
+    },
+    authController.register
+  );
+};
 
 export default route;
