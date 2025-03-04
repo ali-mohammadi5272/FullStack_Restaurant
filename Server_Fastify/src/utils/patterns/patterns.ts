874e1edc94e7e4c7b6a1a2950c9a4020ffd2cb6a
ali-mohammadi5272 <ali.mohammadi5272@gmail.com>
2025-03-04 10:18:34 +0330

    fix: fix userName RegEx Pattern

--- Server_Fastify/src/utils/patterns/patterns.ts
+++ Server_Fastify/src/utils/patterns/patterns.ts
@@ -1,4 +1,4 @@
-const userNamePattern: RegExp = /^[aA-zZ]{1}[aA-zZ0-9]{2,}(_[a-z0-9]+)*?$/;
+const userNamePattern: RegExp = /^[aA-zZ]{1}[aA-zZ0-9]{2,}(_[aA-zZ0-9]+)*?$/;
 const emailPattern: RegExp = /^[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+$/;
 
 export { userNamePattern, emailPattern };
