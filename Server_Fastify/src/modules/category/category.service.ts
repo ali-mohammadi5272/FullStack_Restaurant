166c6dec3efff301188aaabbfcef332dce5000d5
ali-mohammadi5272 <ali.mohammadi5272@gmail.com>
2025-03-06 13:09:19 +0330

    feat: create getOneByTitle Service

--- Server_Fastify/src/modules/category/category.service.ts
+++ Server_Fastify/src/modules/category/category.service.ts
@@ -1,3 +1,11 @@
-const service = {};
+import Category from "./category.model";
+
+const service = {
+  async getOneByTitle(title: string) {
+    return await Category.findOne({
+      where: { title },
+    });
+  },
+};
 
 export default service;
