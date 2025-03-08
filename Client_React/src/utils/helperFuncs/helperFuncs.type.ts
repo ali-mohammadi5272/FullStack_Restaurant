367517e912b2bfa3835687ecae0e238d03cda244
ali-mohammadi5272 <ali.mohammadi5272@gmail.com>
2025-03-08 08:41:58 +0330

    refactor: change CookieType

--- Client_React/src/utils/helperFuncs/helperFuncs.type.ts
+++ Client_React/src/utils/helperFuncs/helperFuncs.type.ts
@@ -2,10 +2,10 @@ export enum CookieEnum {
   REFRESH_TOKEN = "REFRESH_TOKEN",
   ACCESS_TOKEN = "ACCESS_TOKEN",
 }
-  key: string | number;
-  value: string | number;
+
+export interface CookieType {
+  key: CookieEnum;
+  value: string;
   maxAge: number;
   path: string;
 }
-
-export type { CookieType };
