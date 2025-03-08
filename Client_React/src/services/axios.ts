b9031bf2c5b07c2c96c981bfdbd14b47d3cbb242
ali-mohammadi5272 <ali.mohammadi5272@gmail.com>
2025-03-08 08:45:10 +0330

    feat: create axiosInstanceWithHeader

--- Client_React/src/services/axios.ts
+++ Client_React/src/services/axios.ts
@@ -1,7 +1,16 @@
 import axios, { AxiosInstance } from "axios";
+import { getCookie } from "../utils/helperFuncs/helperFuncs.ts";
+import { CookieEnum } from "../utils/helperFuncs/helperFuncs.type.ts";
 
 const axiosInstance: AxiosInstance = axios.create({
   baseURL: import.meta.env.VITE_BASE_URL,
 });
 
-export default axiosInstance;
+const axiosInstanceWithHeader: AxiosInstance = axios.create({
+  baseURL: import.meta.env.VITE_BASE_URL,
+  headers: {
+    Authorization: `Bearer ${getCookie(CookieEnum.ACCESS_TOKEN)}`,
+  },
+});
+
+export { axiosInstance, axiosInstanceWithHeader };
