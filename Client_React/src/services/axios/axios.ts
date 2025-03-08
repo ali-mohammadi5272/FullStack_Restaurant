ef9fff1825ccf510a1e6a76f4f3991634d780947
ali-mohammadi5272 <ali.mohammadi5272@gmail.com>
2025-03-08 11:39:20 +0330

    feat: add interceptors

--- Client_React/src/services/axios/axios.ts
+++ Client_React/src/services/axios/axios.ts
@@ -5,6 +5,7 @@ import {
   createServices,
   getCookie,
 } from "../../utils/helperFuncs/helperFuncs.ts";
+import { toast } from "react-toastify";
 
 const axiosInstance: AxiosInstance = axios.create({
   baseURL: import.meta.env.VITE_BASE_URL,
@@ -16,6 +17,26 @@ const axiosInstanceWithHeader: AxiosInstance = axios.create({
     Authorization: `Bearer ${getCookie(CookieEnum.ACCESS_TOKEN)}`,
   },
 });
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    return config;
+  },
+  (err) => {
+    toast.error(err.response.data.message);
+    return Promise.reject(err);
+  },
+);
+
+axiosInstanceWithHeader.interceptors.request.use(
+  (config) => {
+    return config;
+  },
+  (err) => {
+    toast.error(err.response.data.message);
+    return Promise.reject(err);
+  },
+);
 
 const [request, requestWithHeader]: RequestsObject[] = createServices([
   axiosInstance,
