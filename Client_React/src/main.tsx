c590e71a47bc50cb2ad6b9967db04443943c242a
ali-mohammadi5272 <ali.mohammadi5272@gmail.com>
2025-03-08 10:50:38 +0330

    feat: add ToastContainer

--- Client_React/src/main.tsx
+++ Client_React/src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
 import "./assets/styles/main.scss";
 import OuterContainer from "./components/OuterContainer/OuterContainer.tsx";
+import { ToastContainer } from "react-toastify";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
@@ -11,6 +12,7 @@ createRoot(document.getElementById("root")!).render(
       <OuterContainer>
         <App />
       </OuterContainer>
+      <ToastContainer />
     </BrowserRouter>
   </StrictMode>
 );
