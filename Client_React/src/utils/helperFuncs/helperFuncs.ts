8b67eac1d625755f14216907ef321af945bea578
ali-mohammadi5272 <ali.mohammadi5272@gmail.com>
2025-03-08 10:44:34 +0330

    feat: create createServices function

--- Client_React/src/utils/helperFuncs/helperFuncs.ts
+++ Client_React/src/utils/helperFuncs/helperFuncs.ts
@@ -1,4 +1,6 @@
 import { CookieEnum, CookieType } from "./helperFuncs.type.ts";
+import { AxiosInstance, AxiosResponse } from "axios";
+import { Request, RequestWithBody } from "../../services/axios/axios.type.ts";
 
 const setCookie = (cookie: CookieType) => {
   const { key, value, maxAge, path } = cookie;
@@ -13,4 +15,31 @@ const getCookie = (key: CookieEnum): string | undefined => {
     ?.split(`${key}=`)[1];
 };
 
-export { setCookie, getCookie };
+const createServices = (instances: AxiosInstance[]) =>
+  instances.map((instance) => ({
+    GET: async <D>(req: Request) => {
+      return await instance.get<D, AxiosResponse<D>>(req.url, req.configs);
+    },
+
+    DELETE: async <D>(req: Request) => {
+      return await instance.delete<D, AxiosResponse<D>>(req.url, req.configs);
+    },
+
+    POST: async <D, B>(req: RequestWithBody<B>) => {
+      return await instance.post<D, AxiosResponse<D>, B>(
+        req.url,
+        req.body,
+        req.configs,
+      );
+    },
+
+    PUT: async <D, B>(req: RequestWithBody<B>) => {
+      return await instance.post<D, AxiosResponse<D>, B>(
+        req.url,
+        req.body,
+        req.configs,
+      );
+    },
+  }));
+
+export { setCookie, getCookie, createServices };
