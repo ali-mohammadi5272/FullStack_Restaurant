0587f5d5412eb44d8d378552133e1379ad59db19
ali-mohammadi5272 <ali.mohammadi5272@gmail.com>
2025-03-09 14:34:02 +0330

    feat: create getAll controller

--- Server_Fastify/src/modules/category/category.controller.ts
+++ Server_Fastify/src/modules/category/category.controller.ts
@@ -32,6 +32,24 @@ const controller = {
       });
     }
   },
+
+  async getAll(_: FastifyRequest, res: FastifyReply) {
+    try {
+      const categories = await categoryService.getAll();
+
+      return res.status(200).send({
+        statusCode: 200,
+        messages: [],
+        data: categories,
+      });
+    } catch (error) {
+      return res.status(500).send({
+        statusCode: 500,
+        error,
+        messages: ["Internal Server Error"],
+      });
+    }
+  },
 };
 
 export default controller;
