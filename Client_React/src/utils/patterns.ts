4459e7a4743c3872f45195d25348919c2ea76190
ali-mohammadi5272 <ali.mohammadi5272@gmail.com>
2025-03-09 12:45:00 +0330

    feat: add userName RegEx pattern

--- Client_React/src/utils/patterns.ts
+++ Client_React/src/utils/patterns.ts
@@ -1,3 +1,4 @@
 const authLayoutRoutePattern: RegExp = /^\/auth\/?$/;
+const userNamePattern: RegExp = /^[aA-zZ]{1}[aA-zZ0-9]{2,}(_[aA-zZ0-9]+)*?$/;
 
-export { authLayoutRoutePattern };
+export { authLayoutRoutePattern, userNamePattern };
