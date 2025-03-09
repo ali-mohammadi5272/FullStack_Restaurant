2a90ade840e902b4d728d843121b0218615104c0
ali-mohammadi5272 <ali.mohammadi5272@gmail.com>
2025-03-09 14:32:32 +0330

    feat: get categories from server

--- Client_React/src/components/Categories/Categories.tsx
+++ Client_React/src/components/Categories/Categories.tsx
@@ -1,16 +1,56 @@
-import React, { memo, useState } from "react";
-import { CategoryType } from "./categories.types";
+import React, { memo, useEffect, useMemo, useState } from "react";
 import CustomButton from "../CustomButton/CustomButton";
+import { CategoryType } from "./categories.types";
+import { request } from "../../services/axios/axios";
 
 const Categories = (): React.ReactNode => {
   const [activeCategoryId, setActiveCategoryId] = useState<number>(1);
-  const categories: CategoryType[] = [
-    { id: 1, title: "All catagory" },
-    { id: 2, title: "Dinner" },
-    { id: 3, title: "Lunch" },
-    { id: 4, title: "Dessert" },
-    { id: 5, title: "Drink" },
-  ];
+  const [categories, setCategories] = useState<CategoryType[]>([
+    {
+      id: 0.12345,
+      title: "All catagory",
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+  ]);
+
+  const memoCategories: JSX.Element[] = useMemo(() => {
+    return categories.map((category) => (
+      <CustomButton
+        key={category.id}
+        title={category.title}
+        onClick={() => categoryClickHandler(category)}
+        className={`
+          flex-grow py-7 px-10 md:px-0
+          ${
+            category.id === activeCategoryId
+              ? "bg-[#311F09] text-[#FFFFFF]"
+              : "bg-gray-50 text-[#311F09]"
+          }
+        `}
+      />
+    ));
+  }, [categories]);
+
+  const getCategories = async () => {
+    const response = await request.GET<CategoryType[]>({
+      url: "/categories",
+    });
+    console.log(response.data.data);
+    setCategories([
+      {
+        id: 0.12345,
+        title: "All catagory",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      ...response.data.data,
+    ]);
+  };
+
+  useEffect(() => {
+    getCategories();
+  }, []);
 
   const changeActiveCategory = (categoryId: number): void => {
     setActiveCategoryId(categoryId);
@@ -22,21 +62,7 @@ const Categories = (): React.ReactNode => {
 
   return (
     <div className="flex justify-between gap-5 md:gap-10 my-20 overflow-x-auto">
-      {categories.map((category) => (
-        <CustomButton
-          key={category.id}
-          title={category.title}
-          onClick={() => categoryClickHandler(category)}
-          className={`
-          flex-grow py-7 px-10 md:px-0
-          ${
-            category.id === activeCategoryId
-              ? "bg-[#311F09] text-[#FFFFFF]"
-              : "bg-gray-50 text-[#311F09]"
-          }
-        `}
-        />
-      ))}
+      {memoCategories}
     </div>
   );
 };
