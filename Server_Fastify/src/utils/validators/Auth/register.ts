8486c332d10d309be03ae9839c9b63c5ef5a2c99
ali-mohammadi5272 <ali.mohammadi5272@gmail.com>
2025-03-09 12:19:52 +0330

    feat: add fullName to schema

--- Server_Fastify/src/utils/validators/Auth/register.ts
+++ Server_Fastify/src/utils/validators/Auth/register.ts
@@ -1,7 +1,8 @@
 import { object, string } from "yup";
-import { userNamePattern } from "../../patterns/patterns";
+import { fullNamePattern, userNamePattern } from "../../patterns/patterns";
 
 const registerSchema = object().shape({
+  fullName: string().min(4).required().matches(fullNamePattern),
   userName: string().min(4).required().matches(userNamePattern),
   email: string().min(5).email().required(),
   password: string().min(8).required(),
