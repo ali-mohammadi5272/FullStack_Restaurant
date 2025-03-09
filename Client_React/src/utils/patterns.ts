33031c843f61f0bdd3ef1e1679329e2557a2c7f5
ali-mohammadi5272 <ali.mohammadi5272@gmail.com>
2025-03-09 12:45:26 +0330

    feat: add fullName RegEx pattern

--- Client_React/src/utils/patterns.ts
+++ Client_React/src/utils/patterns.ts
@@ -1,4 +1,5 @@
 const authLayoutRoutePattern: RegExp = /^\/auth\/?$/;
 const userNamePattern: RegExp = /^[aA-zZ]{1}[aA-zZ0-9]{2,}(_[aA-zZ0-9]+)*?$/;
+const fullNamePattern: RegExp = /^[aA-zA]+( [aA-zA]+)?$/;
 
-export { authLayoutRoutePattern, userNamePattern };
+export { authLayoutRoutePattern, userNamePattern, fullNamePattern };
