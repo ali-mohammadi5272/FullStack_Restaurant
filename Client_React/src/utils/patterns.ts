496828d1dafe2156acd6b0405a80c6f91f20b191
ali-mohammadi5272 <ali.mohammadi5272@gmail.com>
2025-03-09 12:45:42 +0330

    feat: add email RegEx pattern

--- Client_React/src/utils/patterns.ts
+++ Client_React/src/utils/patterns.ts
@@ -1,5 +1,11 @@
 const authLayoutRoutePattern: RegExp = /^\/auth\/?$/;
 const userNamePattern: RegExp = /^[aA-zZ]{1}[aA-zZ0-9]{2,}(_[aA-zZ0-9]+)*?$/;
 const fullNamePattern: RegExp = /^[aA-zA]+( [aA-zA]+)?$/;
+const emailPattern: RegExp = /^[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+$/;
 
-export { authLayoutRoutePattern, userNamePattern, fullNamePattern };
+export {
+  authLayoutRoutePattern,
+  userNamePattern,
+  fullNamePattern,
+  emailPattern,
+};
