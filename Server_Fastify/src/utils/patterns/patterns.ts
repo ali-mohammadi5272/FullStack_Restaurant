ef2c950ecab19331884c89c24842ad21fe79adea
ali-mohammadi5272 <ali.mohammadi5272@gmail.com>
2025-03-09 11:56:44 +0330

    feat: create userName RegEx Pattern

--- Server_Fastify/src/utils/patterns/patterns.ts
+++ Server_Fastify/src/utils/patterns/patterns.ts
@@ -1,4 +1,5 @@
 const userNamePattern: RegExp = /^[aA-zZ]{1}[aA-zZ0-9]{2,}(_[aA-zZ0-9]+)*?$/;
+const fullNamePattern: RegExp = /^[aA-zA]+( [aA-zA]+)?$/;
 const emailPattern: RegExp = /^[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+$/;
 
-export { userNamePattern, emailPattern };
+export { userNamePattern, fullNamePattern, emailPattern };
