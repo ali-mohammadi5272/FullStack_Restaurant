1dd6fd249aec0b979a61837ebdcbe7ec8e01f86a
ali-mohammadi5272 <ali.mohammadi5272@gmail.com>
2025-03-09 17:18:15 +0330

    feat: upgrade axios service for caching in GET Request

--- Client_React/src/utils/helperFuncs/helperFuncs.ts
+++ Client_React/src/utils/helperFuncs/helperFuncs.ts
@@ -1,6 +1,10 @@
 import { CookieEnum, CookieType } from "./helperFuncs.type.ts";
 import { AxiosInstance, AxiosResponse } from "axios";
-import { Request, RequestWithBody } from "../../services/axios/axios.type.ts";
+import {
+  Request,
+  RequestsObject,
+  RequestWithBody,
+} from "../../services/axios/axios.type.ts";
 
 const setCookie = (cookie: CookieType) => {
   const { key, value, maxAge, path } = cookie;
@@ -15,31 +19,91 @@ const getCookie = (key: CookieEnum): string | undefined => {
     ?.split(`${key}=`)[1];
 };
 
-const createServices = (instances: AxiosInstance[]) =>
-  instances.map((instance) => ({
-    GET: async <D>(req: Request) => {
-      return await instance.get<D, AxiosResponse<D>>(req.url, req.configs);
-    },
+const createServices = (instances: AxiosInstance[]): RequestsObject[] =>
+  instances.map(
+    (instance): RequestsObject => ({
+      GET: async <D>(req: Request) => {
+        if (!req.cache) {
+          const response = await instance.get<D, AxiosResponse<D>>(
+            req.url,
+            req.configs
+          );
+          return response;
+        }
 
-    DELETE: async <D>(req: Request) => {
-      return await instance.delete<D, AxiosResponse<D>>(req.url, req.configs);
-    },
+        const cachedResponse = sessionStorage.getItem(req.cache.key);
 
-    POST: async <D, B>(req: RequestWithBody<B>) => {
-      return await instance.post<D, AxiosResponse<D>, B>(
-        req.url,
-        req.body,
-        req.configs
-      );
-    },
+        if (!(req.cache.revalidate && req.cache.revalidate > 0)) {
+          if (!cachedResponse) {
+            const response = await instance.get<D, AxiosResponse<D>>(
+              req.url,
+              req.configs
+            );
+            sessionStorage.setItem(req.cache.key, JSON.stringify(response));
 
-    PUT: async <D, B>(req: RequestWithBody<B>) => {
-      return await instance.post<D, AxiosResponse<D>, B>(
-        req.url,
-        req.body,
-        req.configs
-      );
-    },
-  }));
+            return response;
+          }
+
+          return JSON.parse(cachedResponse);
+        }
+
+        if (!cachedResponse) {
+          const response = await instance.get<D, AxiosResponse<D>>(
+            req.url,
+            req.configs
+          );
+          sessionStorage.setItem(req.cache.key, JSON.stringify(response));
+          sessionStorage.setItem(
+            `${req.cache.key}-revalidate`,
+            JSON.stringify(Date.now() + req.cache.revalidate)
+          );
+          return response;
+        }
+
+        const cachedResponseRevalidate = sessionStorage.getItem(
+          `${req.cache.key}-revalidate`
+        );
+
+        const needRevalidate = !(
+          cachedResponseRevalidate &&
+          JSON.parse(cachedResponseRevalidate) > Date.now()
+        );
+        if (!needRevalidate) {
+          return JSON.parse(cachedResponse);
+        }
+
+        const response = await instance.get<D, AxiosResponse<D>>(
+          req.url,
+          req.configs
+        );
+        sessionStorage.setItem(req.cache.key, JSON.stringify(response));
+        sessionStorage.setItem(
+          `${req.cache.key}-revalidate`,
+          JSON.stringify(Date.now() + req.cache.revalidate)
+        );
+        return response;
+      },
+
+      DELETE: async <D>(req: Omit<Request, "cache">) => {
+        return await instance.delete<D, AxiosResponse<D>>(req.url, req.configs);
+      },
+
+      POST: async <D, B>(req: RequestWithBody<B>) => {
+        return await instance.post<D, AxiosResponse<D>, B>(
+          req.url,
+          req.body,
+          req.configs
+        );
+      },
+
+      PUT: async <D, B>(req: RequestWithBody<B>) => {
+        return await instance.post<D, AxiosResponse<D>, B>(
+          req.url,
+          req.body,
+          req.configs
+        );
+      },
+    })
+  );
 
 export { setCookie, getCookie, createServices };
