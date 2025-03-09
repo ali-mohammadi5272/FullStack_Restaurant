89a09543c543793daf49e4b4d4580347b54eae36
ali-mohammadi5272 <ali.mohammadi5272@gmail.com>
2025-03-09 15:33:01 +0330

    refactor: prettier

--- Client_React/src/services/axios/axios.type.ts
+++ Client_React/src/services/axios/axios.type.ts
@@ -14,11 +14,11 @@ interface ErrorResponse {
 
 interface RequestsObject {
   POST: <D, B>(
-    req: RequestWithBody<B>,
+    req: RequestWithBody<B>
   ) => Promise<AxiosResponse<SuccessResponse<D>>>;
 
   PUT: <D, B>(
-    req: RequestWithBody<B>,
+    req: RequestWithBody<B>
   ) => Promise<AxiosResponse<SuccessResponse<D>>>;
 
   DELETE: <D>(req: Request) => Promise<AxiosResponse<SuccessResponse<D>>>;
