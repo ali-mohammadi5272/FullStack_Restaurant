a64d624dccefb7b114bcecebd10c2dcdcffe3e28
ali-mohammadi5272 <ali.mohammadi5272@gmail.com>
2025-03-10 08:31:22 +0330

    refactor: change import

--- Server_Fastify/app.ts
+++ Server_Fastify/app.ts
@@ -1,6 +1,6 @@
 import fastify from "fastify";
 import cors from "@fastify/cors";
-import authRouter from "./src/modules/auth/auth.route";
+import authRouter from "./src/modules/auth/router";
 import categoryRouter from "./src/modules/category/router";
 import { env } from "./src/utils/env/env";
 import { sequelize } from "./src/configs/db";
