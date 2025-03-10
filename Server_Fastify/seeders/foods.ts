c2d542f98f28ce7f2dd5fb55f0df503988123fdf
ali-mohammadi5272 <ali.mohammadi5272@gmail.com>
2025-03-10 13:15:59 +0330

    feat: create Food Seeder

---
+++ Server_Fastify/seeders/foods.ts
@@ -0,0 +1,9 @@
+import Food from "../src/modules/Food/model";
+import { db } from "../MockData/db";
+
+const seed = async () => {
+  await Food.bulkCreate(db.foods);
+  console.log("✅ Foods have been seeded!");
+};
+
+export default seed;
