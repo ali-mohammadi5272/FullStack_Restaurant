049eaf68261a8d0a82132fd2aa95561a98a98bef
ali-mohammadi5272 <ali.mohammadi5272@gmail.com>
2025-03-11 21:48:01 +0330

    feat: create createOne service

--- Server_Fastify/src/modules/FoodCategory/service.ts
+++ Server_Fastify/src/modules/FoodCategory/service.ts
@@ -1,3 +1,10 @@
-const service = {};
+import { CreateOneDto } from "./dto/create-one.dto";
+import FoodCategory from "./model";
+
+const service = {
+  async createOne(body: CreateOneDto) {
+    return await FoodCategory.create(body);
+  },
+};
 
 export default service;
