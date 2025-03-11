3fab76f38a040711afe5ad333c9cf3106ae13930
ali-mohammadi5272 <ali.mohammadi5272@gmail.com>
2025-03-11 16:45:08 +0330

    feat: create createOne food validator

---
+++ Server_Fastify/src/utils/validators/Food/createOne.ts
@@ -0,0 +1,14 @@
+import { array, mixed, number, object, string } from "yup";
+import { FoodTypes } from "../../../modules/Food/enum/foodTypes.enum";
+
+const createFoodSchema = object().shape({
+  title: string().min(2).required(),
+  price: number().min(0).required(),
+  description: string().min(2).required(),
+  categories: array().of(number().required()).required(),
+  foodType: mixed<FoodTypes>()
+    .oneOf([FoodTypes.PASTA, FoodTypes.PIZZA, FoodTypes.RICE, FoodTypes.SOUP])
+    .required(),
+});
+
+export default createFoodSchema;
