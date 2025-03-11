ff1297fa430a2cc0d36d59e1069ba34652817a50
ali-mohammadi5272 <ali.mohammadi5272@gmail.com>
2025-03-11 22:30:39 +0330

    feat: create createOne router

--- Server_Fastify/src/modules/Food/router.ts
+++ Server_Fastify/src/modules/Food/router.ts
@@ -1,3 +1,36 @@
-const router = () => {};
+import foodController from "./controller";
+import createFoodSchema from "../../utils/validators/Food/createOne";
+import { FastifyInstance } from "fastify";
+import { formDataValidator } from "../../utils/middlewares/formDataValidator";
+import { CreateOneDtoType } from "./dto/create-one.dto";
+import { ImageFormats } from "./enum/imageFormats.enum";
+import { fileValidator } from "../../utils/middlewares/fileValidator";
+import { auth } from "../../utils/middlewares/auth";
+import { roleAccess } from "../../utils/middlewares/roleAccess";
+import { Roles } from "../User/enum/roles.enum";
+
+const router = (server: FastifyInstance) => {
+  server.post(
+    "/",
+    {
+      preHandler: [
+        auth,
+        roleAccess([Roles.ADMIN]),
+        formDataValidator<Omit<CreateOneDtoType, "image">>(createFoodSchema, [
+          "price",
+          "categories",
+        ]),
+        fileValidator([
+          ImageFormats.jpeg,
+          ImageFormats.jpg,
+          ImageFormats.png,
+          ImageFormats.svg,
+          ImageFormats.webp,
+        ]),
+      ],
+    },
+    foodController.createOne
+  );
+};
 
 export default router;
