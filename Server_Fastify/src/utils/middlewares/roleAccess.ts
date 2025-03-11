4e51577b748bad9703d68772a30df0ccc765438f
ali-mohammadi5272 <ali.mohammadi5272@gmail.com>
2025-03-11 17:29:14 +0330

    feat: create roleAccess middleware

---
+++ Server_Fastify/src/utils/middlewares/roleAccess.ts
@@ -0,0 +1,16 @@
+import { FastifyReply } from "fastify";
+import { Roles } from "../../modules/User/enum/roles.enum";
+import { AuthenticatedRequest } from "./auth";
+
+const roleAccess =
+  (roles: Roles[]) => async (req: AuthenticatedRequest, res: FastifyReply) => {
+    if (!req.user || !roles.includes(req.user.role)) {
+      return res.status(403).send({
+        statusCode: 403,
+        error: "Forbidden",
+        messages: ["Forbidden"],
+      });
+    }
+  };
+
+module.exports = { roleAccess };
