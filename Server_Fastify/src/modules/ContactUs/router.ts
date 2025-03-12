46d68fe17117e9208fc247f1ddc75a42e8afe990
ali-mohammadi5272 <ali.mohammadi5272@gmail.com>
2025-03-12 14:35:46 +0330

    feat: add createOne  controller to router

--- Server_Fastify/src/modules/ContactUs/router.ts
+++ Server_Fastify/src/modules/ContactUs/router.ts
@@ -3,8 +3,17 @@ import createContactSchema from "../../utils/validators/Contact-us/createOne";
 import { FastifyInstance } from "fastify";
 import { bodyValidator } from "../../utils/middlewares/bodyValidator";
 import { CreateOneDto } from "./dto/create-one.dto";
+import { auth } from "../../utils/middlewares/auth";
+import { roleAccess } from "../../utils/middlewares/roleAccess";
+import { Roles } from "../User/enum/roles.enum";
 
 const router = (server: FastifyInstance) => {
+  server.get(
+    "/",
+    { preHandler: [auth, roleAccess([Roles.ADMIN])] },
+    controller.getAll
+  );
+
   server.post(
     "/",
     {
