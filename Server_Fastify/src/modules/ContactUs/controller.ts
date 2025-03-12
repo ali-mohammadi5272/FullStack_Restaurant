00f0387ec21c692d350d126b080fa80d7c3309b5
ali-mohammadi5272 <ali.mohammadi5272@gmail.com>
2025-03-12 14:27:28 +0330

    feat: create createOne Contact-us controller

--- Server_Fastify/src/modules/ContactUs/controller.ts
+++ Server_Fastify/src/modules/ContactUs/controller.ts
@@ -1,3 +1,35 @@
-const controller = {};
+import { FastifyReply, FastifyRequest } from "fastify";
+import { CreateOneDto as CreateContactOneDto } from "./dto/create-one.dto";
+import contactService from "./service";
+
+const controller = {
+  async createOne(
+    req: FastifyRequest<{ Body: CreateContactOneDto }>,
+    res: FastifyReply
+  ) {
+    try {
+      const contact = await contactService.createOne(req.body);
+      if (!contact) {
+        return res.status(500).send({
+          statusCode: 500,
+          error: "Internal Server Error",
+          messages: ["Internal Server Error"],
+        });
+      }
+
+      return res.status(201).send({
+        statusCode: 201,
+        data: null,
+        messages: ["Message sent successfully"],
+      });
+    } catch (error) {
+      return res.status(500).send({
+        statusCode: 500,
+        error: "Internal Server Error",
+        messages: ["Internal Server Error"],
+      });
+    }
+  },
+};
 
 export default controller;
