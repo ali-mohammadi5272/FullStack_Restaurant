7ee241a86d173c28b81ca234a3f7892b5a91dd80
ali-mohammadi5272 <ali.mohammadi5272@gmail.com>
2025-03-12 14:31:33 +0330

    feat: create findAll service

--- Server_Fastify/src/modules/ContactUs/service.ts
+++ Server_Fastify/src/modules/ContactUs/service.ts
@@ -3,6 +3,10 @@ import { AnsweredTypes } from "./enum/answered.enum";
 import Contact from "./model";
 
 const service = {
+  async getAll() {
+    return await Contact.findAll({});
+  },
+
   async createOne(body: CreateOneDto) {
     return await Contact.create({ ...body, answered: AnsweredTypes.PENDING });
   },
