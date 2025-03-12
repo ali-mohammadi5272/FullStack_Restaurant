6f7d97c71182056144fa4ffffc28bf00753dbc95
ali-mohammadi5272 <ali.mohammadi5272@gmail.com>
2025-03-12 12:52:31 +0330

    feat: create ContactUs Model

---
+++ Server_Fastify/src/modules/ContactUs/model.ts
@@ -0,0 +1,78 @@
+import { sequelize } from "../../configs/db";
+import { AnsweredTypes } from "./enum/Answered.enum";
+import {
+  CreationOptional,
+  DataTypes,
+  InferAttributes,
+  InferCreationAttributes,
+  Model,
+} from "sequelize";
+
+class Contact extends Model<
+  InferAttributes<Contact>,
+  InferCreationAttributes<Contact>
+> {
+  declare id: CreationOptional<number>;
+  declare firstName: string;
+  declare lastName: string;
+  declare email: string;
+  declare subject: string;
+  declare message: string;
+  declare answered: AnsweredTypes;
+  declare createdAt: CreationOptional<Date>;
+  declare updatedAt: CreationOptional<Date>;
+}
+
+Contact.init(
+  {
+    id: {
+      type: DataTypes.BIGINT.UNSIGNED,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    subject: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    message: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    answered: {
+      type: DataTypes.ENUM(
+        AnsweredTypes.PENDING,
+        AnsweredTypes.ANSWERED,
+        AnsweredTypes.REJECTED
+      ),
+      allowNull: false,
+      defaultValue: AnsweredTypes.PENDING,
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: new Date(),
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: new Date(),
+    },
+  },
+  {
+    tableName: "contacts",
+    timestamps: true,
+    sequelize,
+  }
+);
