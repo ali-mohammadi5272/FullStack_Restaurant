a0347dbadf1511c36bbfad304326b34be87bc1fa
ali-mohammadi5272 <ali.mohammadi5272@gmail.com>
2025-03-12 14:28:49 +0330

    feat: add createOne  Contact-us to  router

--- Server_Fastify/src/modules/ContactUs/router.ts
+++ Server_Fastify/src/modules/ContactUs/router.ts
@@ -1,3 +1,17 @@
-const router = () => {};
+import controller from "./controller";
+import createContactSchema from "../../utils/validators/Contact-us/createOne";
+import { FastifyInstance } from "fastify";
+import { bodyValidator } from "../../utils/middlewares/bodyValidator";
+import { CreateOneDto } from "./dto/create-one.dto";
+
+const router = (server: FastifyInstance) => {
+  server.post(
+    "/",
+    {
+      preHandler: [bodyValidator<CreateOneDto>(createContactSchema)],
+    },
+    controller.createOne
+  );
+};
 
 export default router;
