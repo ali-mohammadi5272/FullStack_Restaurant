b5cee932a074d195e26824d83cf863791558fdda
ali-mohammadi5272 <ali.mohammadi5272@gmail.com>
2025-03-12 14:26:15 +0330

    feat: create createOne Contact-us service

--- Server_Fastify/src/modules/ContactUs/service.ts
+++ Server_Fastify/src/modules/ContactUs/service.ts
@@ -1,3 +1,11 @@
-const service = {};
+import { CreateOneDto } from "./dto/create-one.dto";
+import { AnsweredTypes } from "./enum/answered.enum";
+import Contact from "./model";
+
+const service = {
+  async createOne(body: CreateOneDto) {
+    return await Contact.create({ ...body, answered: AnsweredTypes.PENDING });
+  },
+};
 
 export default service;
