7ea1c5cb1f56012f21b5492c138fb0c4377e0939
ali-mohammadi5272 <ali.mohammadi5272@gmail.com>
2025-03-12 14:58:14 +0330

    feat: create updateOne service

--- Server_Fastify/src/modules/category/service.ts
+++ Server_Fastify/src/modules/category/service.ts
@@ -1,7 +1,14 @@
 import Category from "./model";
 import { CreateOneDtoType } from "./dto/create-one.dto";
+import { UpdateOneDtoType } from "./dto/update-one.dto";
 
 const service = {
+  async getOneById(categoryId: number) {
+    return await Category.findOne({
+      where: { id: categoryId },
+    });
+  },
+
   async getOneByTitle(title: string) {
     return await Category.findOne({
       where: { title },
@@ -14,6 +21,10 @@ const service = {
 
   async createOne(body: CreateOneDtoType) {
     return await Category.create(body);
+  },
+
+  async updateOne(body: UpdateOneDtoType, categoryId: number) {
+    return await Category.update(body, { where: { id: categoryId } });
   },
 };
 
