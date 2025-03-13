5e97630ccc9a4daf9a7d037dd5ac60c27828c688
ali-mohammadi5272 <ali.mohammadi5272@gmail.com>
2025-03-13 15:53:40 +0330

    feat: add updateOne Category to router

--- Server_Fastify/src/modules/Category/router.ts
+++ Server_Fastify/src/modules/Category/router.ts
@@ -1,8 +1,13 @@
 import createCategorySchema from "../../utils/validators/Categories/CreateOne";
 import categoryController from "./controller";
+import updateCategorySchema from "../../utils/validators/Categories/updateOne";
 import { FastifyInstance } from "fastify";
 import { bodyValidator } from "../../utils/middlewares/bodyValidator";
 import { CreateOneDtoType } from "./dto/create-one.dto";
+import { UpdateOneDto, UpdateOneParamsDto } from "./dto/update-one.dto";
+import { auth } from "../../utils/middlewares/auth";
+import { roleAccess } from "../../utils/middlewares/roleAccess";
+import { Roles } from "../User/enum/roles.enum";
 
 const router = (server: FastifyInstance) => {
   server.get("/", categoryController.getAll);
@@ -11,6 +16,18 @@ const router = (server: FastifyInstance) => {
     "/",
     { preHandler: [bodyValidator<CreateOneDtoType>(createCategorySchema)] },
     categoryController.createOne
+  );
+
+  server.put<{ Body: UpdateOneDto; Params: UpdateOneParamsDto }>(
+    "/",
+    {
+      preHandler: [
+        auth,
+        roleAccess([Roles.ADMIN]),
+        bodyValidator<UpdateOneDto>(updateCategorySchema),
+      ],
+    },
+    categoryController.updateOne
   );
 };
 
