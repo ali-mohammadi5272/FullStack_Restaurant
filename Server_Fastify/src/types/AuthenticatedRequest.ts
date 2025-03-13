b3dab98af9856482b66b9e00b64bde80aa668bbb
ali-mohammadi5272 <ali.mohammadi5272@gmail.com>
2025-03-13 12:40:23 +0330

    feat: create AuthenticatedRequest interface

---
+++ Server_Fastify/src/types/AuthenticatedRequest.ts
@@ -0,0 +1,29 @@
+import User from "../modules/User/model";
+import { FastifyRequestType, ResolveFastifyRequestType } from "fastify/types/type-provider";
+import { ContextConfigDefault, FastifyBaseLogger, FastifyRequest, FastifySchema, FastifyTypeProvider, FastifyTypeProviderDefault, RawRequestDefaultExpression, RawServerBase, RawServerDefault, RouteGenericInterface } from "fastify";
+
+export interface AuthenticatedRequest<
+  RouteGeneric extends RouteGenericInterface = RouteGenericInterface,
+  RawServer extends RawServerBase = RawServerDefault,
+  RawRequest extends RawRequestDefaultExpression<RawServer> = RawRequestDefaultExpression<RawServer>,
+  SchemaCompiler extends FastifySchema = FastifySchema,
+  TypeProvider extends FastifyTypeProvider = FastifyTypeProviderDefault,
+  ContextConfig = ContextConfigDefault,
+  Logger extends FastifyBaseLogger = FastifyBaseLogger,
+  RequestType extends FastifyRequestType = ResolveFastifyRequestType<
+    TypeProvider,
+    SchemaCompiler,
+    RouteGeneric
+  >
+> extends FastifyRequest<
+    RouteGeneric,
+    RawServer,
+    RawRequest,
+    SchemaCompiler,
+    TypeProvider,
+    ContextConfig,
+    Logger,
+    RequestType
+  > {
+  user?: User;
+}
