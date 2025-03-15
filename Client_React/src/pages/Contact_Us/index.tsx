5cb3105273cd16aebdc579197fc6b16ccbb48ce8
ali-mohammadi5272 <ali.mohammadi5272@gmail.com>
2025-03-15 11:12:35 +0330

    feat: create ContactUsPage

---
+++ Client_React/src/pages/Contact_Us/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Contact from "./Contact";
+
+const ContactUsPage = (): React.ReactNode => {
+  return (
+    <main>
+      <Contact />
+    </main>
+  );
+};
+
+export default ContactUsPage;
+1;
