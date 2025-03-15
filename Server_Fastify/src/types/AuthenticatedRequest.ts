bc3395d0f4c04ef6641d918a77e2e676a87339b3
ali-mohammadi5272 <ali.mohammadi5272@gmail.com>
2025-03-15 14:06:37 +0330

    Revert "refactor: user is required"
    This reverts commit 293839ae91ea9177a398a73b4febd604ced7c68b.

--- Server_Fastify/src/types/AuthenticatedRequest.ts
+++ Server_Fastify/src/types/AuthenticatedRequest.ts
@@ -1,20 +1,6 @@
 import User from "../modules/User/model";
-import {
-  FastifyRequestType,
-  ResolveFastifyRequestType,
-} from "fastify/types/type-provider";
-import {
-  ContextConfigDefault,
-  FastifyBaseLogger,
-  FastifyRequest,
-  FastifySchema,
-  FastifyTypeProvider,
-  FastifyTypeProviderDefault,
-  RawRequestDefaultExpression,
-  RawServerBase,
-  RawServerDefault,
-  RouteGenericInterface,
-} from "fastify";
+import { FastifyRequestType, ResolveFastifyRequestType } from "fastify/types/type-provider";
+import { ContextConfigDefault, FastifyBaseLogger, FastifyRequest, FastifySchema, FastifyTypeProvider, FastifyTypeProviderDefault, RawRequestDefaultExpression, RawServerBase, RawServerDefault, RouteGenericInterface } from "fastify";
 
 export interface AuthenticatedRequest<
   RouteGeneric extends RouteGenericInterface = RouteGenericInterface,
@@ -39,5 +25,5 @@ export interface AuthenticatedRequest<
     Logger,
     RequestType
   > {
-  user: User;
+  user?: User;
 }
