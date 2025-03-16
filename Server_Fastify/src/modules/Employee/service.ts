f2a02b09c117eb77ea9bccecfaa4c5b1b84b0328
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2025-03-16 18:34:04 +0330

    feat: create getOneByFirstNameAndLastName service

--- Server_Fastify/src/modules/Employee/service.ts
+++ Server_Fastify/src/modules/Employee/service.ts
@@ -5,6 +5,17 @@ const service = {
   async createOne(body: CreateOneEmployeeDto) {
     return await Employee.create(body);
   },
+
+  async getOneByFirstNameAndLastName(
+    employee: Pick<Employee, "firstName" | "lastName">
+  ) {
+    await Employee.findOne({
+      where: {
+        firstName: employee.firstName,
+        lastName: employee.lastName,
+      },
+    });
+  },
 };
 
 export default service;
