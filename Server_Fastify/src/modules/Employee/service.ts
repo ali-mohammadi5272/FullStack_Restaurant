de50a9682835a8601eeec20e095d568cfdc1c1bd
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2025-03-16 17:04:13 +0330

    feat: createOne service

--- Server_Fastify/src/modules/Employee/service.ts
+++ Server_Fastify/src/modules/Employee/service.ts
@@ -1,3 +1,10 @@
-const service = {};
+import { CreateOneEmployeeDto } from "./dto/create-one";
+import Employee from "./model";
+
+const service = {
+  async createOne(body: CreateOneEmployeeDto) {
+    return await Employee.create(body);
+  },
+};
 
 export default service;
