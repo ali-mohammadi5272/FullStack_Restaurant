ea059e357e9e92a13db24d7ae28198b7cf90b347
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2025-03-16 16:43:05 +0330

    feat: create Employee Model

---
+++ Server_Fastify/src/modules/Employee/model.ts
@@ -0,0 +1,80 @@
+import { EmployeeRoles } from "./enum/employeeRoles.enum";
+import { sequelize } from "../../configs/db";
+import {
+  CreationOptional,
+  DataTypes,
+  InferAttributes,
+  InferCreationAttributes,
+  Model,
+} from "sequelize";
+import { Genders } from "../User/enum/genders.enum";
+
+class Employee extends Model<
+  InferAttributes<Employee>,
+  InferCreationAttributes<Employee>
+> {
+  declare id: CreationOptional<number>;
+  declare firstName: string;
+  declare lastName: string;
+  declare gender: Genders;
+  declare role: EmployeeRoles;
+  declare image: string;
+  declare createdAt: CreationOptional<Date>;
+  declare updatedAt: CreationOptional<Date>;
+}
+
+Employee.init(
+  {
+    id: {
+      type: DataTypes.INTEGER.UNSIGNED,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    gender: {
+      type: DataTypes.ENUM(Genders.FEMALE, Genders.MALE),
+      allowNull: false,
+    },
+
+    role: {
+      type: DataTypes.ENUM(
+        EmployeeRoles.CHEF,
+        EmployeeRoles.HEAD_CHEF,
+        EmployeeRoles.MANAGER,
+        EmployeeRoles.WAITER
+      ),
+      allowNull: false,
+    },
+
+    image: {
+      type: DataTypes.STRING,
+    },
+
+    createdAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: new Date(),
+    },
+
+    updatedAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: new Date(),
+    },
+  },
+  {
+    sequelize,
+    tableName: "employees",
+    timestamps: true,
+  }
+);
