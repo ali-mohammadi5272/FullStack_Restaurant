dd22378068c91943706dfe3f4bd006924312439b
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2025-03-18 11:09:45 +0330

    refactor: change img alt attr value

--- Client_React/src/components/ChefCard/ChefCard.tsx
+++ Client_React/src/components/ChefCard/ChefCard.tsx
@@ -11,7 +11,7 @@ const ChefCard: React.FC<ChefCardPropsType> = ({
     <article className="text-center text-2xl space-y-4 sm:space-y-8">
       <img
         src={image}
-        alt="Food's Image"
+        alt="Employee's Image"
         className="w-full h-auto rounded-[50px]"
       />
       <h3 className="text-sm sm:text-lg md:text-2xl font-semibold text-[#311F09]">
