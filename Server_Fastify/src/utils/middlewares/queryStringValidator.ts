922a51b5e075392e626caeb81673c9ad083e61b3
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2025-03-25 13:55:00 +0330

    feat: create queryStringValidator middleware

---
+++ Server_Fastify/src/utils/middlewares/queryStringValidator.ts
@@ -0,0 +1,22 @@
+import { FastifyReply, FastifyRequest } from "fastify";
+import { AnyObject, ObjectSchema, ValidationError } from "yup";
+
+const queryStringValidator =
+  <T extends AnyObject>(schema: ObjectSchema<T>) =>
+  async (req: FastifyRequest<{ Querystring: T }>, res: FastifyReply) => {
+    try {
+      await schema.validate(req.query, {
+        abortEarly: false,
+        stripUnknown: true,
+      });
+    } catch (err) {
+      if (err instanceof ValidationError) {
+        return res.status(400).send({
+          statusCode: 400,
+          messages: err.errors,
+        });
+      }
+    }
+  };
+
+export { queryStringValidator };
