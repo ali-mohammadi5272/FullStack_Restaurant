e1d01891e28e4ad3df4ffb5c956354e9172f2e3c
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2025-03-25 13:46:10 +0330

    feat: create getAllEmployeesQueryStringSchema

---
+++ Server_Fastify/src/utils/validators/Employee/getAllQueryString.ts
@@ -0,0 +1,21 @@
+import { array, number, object, mixed } from "yup";
+import { EmployeeRoles } from "../../../modules/Employee/enum/employeeRoles.enum";
+
+const employeeRoles = Object.values(EmployeeRoles);
+
+const getAllEmployeesQueryStringSchema = object().shape({
+  limit: number().min(1),
+  offset: number().min(0),
+  "roles[]": array()
+    .of(
+      mixed<EmployeeRoles>()
+        .oneOf(
+          employeeRoles,
+          "Roles must be one of the following values: chef, head chef, manager, waiter"
+        )
+        .required()
+    )
+    .required(),
+});
+
+export default getAllEmployeesQueryStringSchema;
