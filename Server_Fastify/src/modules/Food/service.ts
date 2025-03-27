41d8914527f17ddb8c8beee1b2121a776a60582a
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2025-03-27 18:36:37 +0330

    feat: add categories from createOne Food service

--- Server_Fastify/src/modules/Food/service.ts
+++ Server_Fastify/src/modules/Food/service.ts
@@ -1,10 +1,22 @@
+import categoryService from "../Category/service";
 import { Food } from "../associations";
 import { Category } from "../associations";
 import { CreateOneDtoType } from "./dto/create-one.dto";
 
 const service = {
-  async createOne(body: Omit<CreateOneDtoType, "categories">) {
-    return await Food.create(body);
+  async createOne(body: CreateOneDtoType) {
+    const food = await Food.create({
+      title: body.title,
+      foodType: body.foodType,
+      description: body.description,
+      image: body.image,
+      price: body.price,
+    });
+
+    const categories = await categoryService.getByIds(body.categories);
+    await food.addCategories(categories);
+
+    return food;
   },
 
   async getOneByTitle(title: string) {
