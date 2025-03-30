308b026bd6071c7a2d131d1a217834dc781368bd
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2025-03-30 15:00:13 +0330

    refactor: use logs and change error handlings

--- Server_Fastify/src/utils/middlewares/auth.ts
+++ Server_Fastify/src/utils/middlewares/auth.ts
@@ -1,35 +1,36 @@
 import userService from "./../../modules/User/service";
-import { Unauthorized, NotFound, InternalServerError } from "http-errors";
+import logger from "./../logger/logger";
+import { Unauthorized, NotFound } from "http-errors";
 import { getAccessTokenPayload } from "../helperFuncs/helperFuncs";
 import { AuthenticatedRequest } from "../../types/AuthenticatedRequest.type";
 import { AccessTokenPayloadType } from "../helperFuncs/helperFuncs.type";
 
 const auth = async (req: AuthenticatedRequest) => {
-  try {
-    if (!req.headers.authorization) {
-      throw new Unauthorized();
-    }
-
-    const token = req.headers.authorization.replace("Bearer ", "");
-    if (!token) {
-      throw new Unauthorized();
-    }
-
-    const tokenPayload = getAccessTokenPayload(token);
-    if (!tokenPayload || typeof tokenPayload === "string") {
-      throw new Unauthorized();
-    }
-
-    const user = await userService.getOne(tokenPayload.userId);
-    if (!user) {
-      throw new NotFound("User not found");
-    }
-
-    req.user = user;
-    req.refreshToken = (<AccessTokenPayloadType>tokenPayload).refreshToken;
-  } catch (error) {
-    throw new InternalServerError();
+  if (!req.headers.authorization) {
+    logger.warn("Missing authorization header");
+    throw new Unauthorized();
   }
+
+  const token = req.headers.authorization.replace("Bearer ", "");
+  if (!token) {
+    logger.warn("Empty Bearer token");
+    throw new Unauthorized();
+  }
+
+  const tokenPayload = getAccessTokenPayload(token);
+  if (!tokenPayload || typeof tokenPayload === "string") {
+    logger.warn("Invalid token payload");
+    throw new Unauthorized();
+  }
+
+  const user = await userService.getOne(tokenPayload.userId);
+  if (!user) {
+    logger.warn(`User not found (ID: ${tokenPayload.userId})`);
+    throw new NotFound("User not found");
+  }
+
+  req.user = user;
+  req.refreshToken = (<AccessTokenPayloadType>tokenPayload).refreshToken;
 };
 
 export { auth };
