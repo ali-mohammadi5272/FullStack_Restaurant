1d9efad9ee82670b9ce3f048ea2440df9b35d2e1
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2025-03-30 15:10:39 +0330

    refactor: remove tryCatch

--- Server_Fastify/src/modules/Category/controller.ts
+++ Server_Fastify/src/modules/Category/controller.ts
@@ -2,36 +2,26 @@ import categoryService from "./service";
 import { FastifyReply, FastifyRequest } from "fastify";
 import { CreateOneDtoType } from "./dto/create-one.dto";
 import { UpdateOneDto, UpdateOneParamsDto } from "./dto/update-one.dto";
+import { BadRequest } from "http-errors";
 
 const controller = {
   async createOne(
     req: FastifyRequest<{ Body: CreateOneDtoType }>,
     res: FastifyReply
   ) {
-    try {
-      const isUserExistBefore = !!(await categoryService.getOneByTitle(
-        req.body.title
-      ));
-      if (isUserExistBefore) {
-        return res.status(400).send({
-          statusCode: 400,
-          messages: ["Category already exists"],
-        });
-      }
-
-      const category = await categoryService.createOne(req.body);
-      return res.status(201).send({
-        statusCode: 201,
-        messages: ["Category created successfully"],
-        data: { category },
-      });
-    } catch (error) {
-      return res.status(500).send({
-        statusCode: 500,
-        error,
-        messages: ["Internal Server Error"],
-      });
+    const isUserExistBefore = !!(await categoryService.getOneByTitle(
+      req.body.title
+    ));
+    if (isUserExistBefore) {
+      throw new BadRequest("Category already exists");
     }
+
+    const category = await categoryService.createOne(req.body);
+    return res.status(201).send({
+      statusCode: 201,
+      messages: ["Category created successfully"],
+      data: { category },
+    });
   },
 
   async getAll(_: FastifyRequest, res: FastifyReply) {
