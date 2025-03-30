4a112055a929ab8bc37acc0176c712b4d9f82d7a
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2025-03-30 15:48:28 +0330

    refactor: change createOne controller

--- Server_Fastify/src/modules/Food/controller.ts
+++ Server_Fastify/src/modules/Food/controller.ts
@@ -4,59 +4,49 @@ import foodService from "./service";
 import { FastifyReply, FastifyRequest } from "fastify";
 import { CreateOneDtoType as CreateOneFoodDtoType } from "./dto/create-one.dto";
 import { FoodTypes } from "./enum/foodTypes.enum";
+import { createSuccessResponse } from "../../utils/helperFuncs/helperFuncs";
+import { BadRequest } from "http-errors";
 
 const controller = {
   async createOne(req: FastifyRequest, res: FastifyReply) {
-    try {
-      const formData: FormData = await req.formData();
+    const formData: FormData = await req.formData();
 
-      const title = formData.get("title") as string;
-      const isFoodExistsBefore = await foodService.getOneByTitle(title);
-      if (isFoodExistsBefore) {
-        return res.status(400).send({
-          statusCode: 400,
-          error: "Duplicated Food",
-          messages: ["Food already exists"],
-        });
-      }
-
-      const file = formData.get("file") as File;
-
-      const arrayBuffer = await file.arrayBuffer();
-      const buffer: Buffer = Buffer.from(arrayBuffer);
-
-      const fileName = `${Date.now()}-${Math.random() * 789}-${file.name}`;
-      const pathAddress = path.join(
-        process.cwd(),
-        "public/images/foods/",
-        `${fileName}`
-      );
-
-      fs.writeFileSync(pathAddress, buffer);
-
-      const body: CreateOneFoodDtoType = {
-        title: formData.get("title") as string,
-        price: JSON.parse(formData.get("price") as string) as number,
-        foodType: formData.get("foodType") as FoodTypes,
-        description: formData.get("description") as string,
-        categories: JSON.parse(formData.get("categories") as string),
-        image: fileName,
-      };
-
-      await foodService.createOne(body);
-
-      return res.status(201).send({
-        statusCode: 201,
-        data: null,
-        messages: ["Food created successfully"],
-      });
-    } catch (error) {
-      return res.status(500).send({
-        statusCode: 500,
-        error: "Internal Server Error",
-        messages: ["Internal Server Error"],
-      });
+    const title = formData.get("title") as string;
+    const isFoodExistsBefore = await foodService.getOneByTitle(title);
+    if (isFoodExistsBefore) {
+      throw new BadRequest("Food already exists");
     }
+
+    const file = formData.get("file") as File;
+
+    const arrayBuffer = await file.arrayBuffer();
+    const buffer: Buffer = Buffer.from(arrayBuffer);
+
+    const fileName = `${Date.now()}-${Math.random() * 789}-${file.name}`;
+    const pathAddress = path.join(
+      process.cwd(),
+      "public/images/foods/",
+      `${fileName}`
+    );
+
+    fs.writeFileSync(pathAddress, buffer);
+
+    const body: CreateOneFoodDtoType = {
+      title: formData.get("title") as string,
+      price: JSON.parse(formData.get("price") as string) as number,
+      foodType: formData.get("foodType") as FoodTypes,
+      description: formData.get("description") as string,
+      categories: JSON.parse(formData.get("categories") as string),
+      image: fileName,
+    };
+
+    const food = await foodService.createOne(body);
+
+    return createSuccessResponse(res, {
+      statusCode: 201,
+      data: food,
+      message: "Food created successfully",
+    });
   },
 
   async getAll(_: FastifyRequest, res: FastifyReply) {
