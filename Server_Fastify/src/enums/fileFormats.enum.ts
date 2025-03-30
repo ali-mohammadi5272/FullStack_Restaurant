0566b9676c98825a699753308d1cb23571db45bd
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2025-03-30 17:38:57 +0330

    feat: create fileFormats.enum

---
+++ Server_Fastify/src/enums/fileFormats.enum.ts
@@ -0,0 +1,13 @@
+export enum FileTypes {
+  IMAGE = "Image",
+  VIDEO = "Video",
+  AUDIO = "Audio",
+}
+
+export enum ImageFormats {
+  jpg = "image/jpeg",
+  jpeg = "image/jpeg",
+  svg = "image/svg+xml",
+  png = "image/png",
+  webp = "image/webp",
+}
