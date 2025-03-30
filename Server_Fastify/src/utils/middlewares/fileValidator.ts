a234c53790ae45cdbc62e4b7f860fb9c840d3689
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2025-03-30 16:00:03 +0330

    refactor: change fileValidator middleware

--- Server_Fastify/src/utils/middlewares/fileValidator.ts
+++ Server_Fastify/src/utils/middlewares/fileValidator.ts
@@ -1,30 +1,18 @@
-import { FastifyReply, FastifyRequest } from "fastify";
+import { FastifyRequest } from "fastify";
 import { ImageFormats } from "../../modules/Food/enum/imageFormats.enum";
+import { BadRequest } from "http-errors";
 
 const fileValidator =
-  (validFormats: ImageFormats[]) =>
-  async (req: FastifyRequest, res: FastifyReply) => {
-    try {
-      const formData = await req.formData();
+  (validFormats: ImageFormats[]) => async (req: FastifyRequest) => {
+    const formData = await req.formData();
 
-      const file = formData.get("file") as File | null;
+    const file = formData.get("file") as File | null;
 
-      if (file) {
-        const isValidFormat = validFormats.includes(file.type as ImageFormats);
-        if (!isValidFormat) {
-          return res.status(400).send({
-            statusCode: 400,
-            error: "Valiadtion",
-            messages: ["Image's format is not valid"],
-          });
-        }
+    if (file) {
+      const isValidFormat = validFormats.includes(file.type as ImageFormats);
+      if (!isValidFormat) {
+        throw new BadRequest("Image's Format is not valid");
       }
-    } catch (err) {
-      return res.status(500).send({
-        statusCode: 500,
-        error: "Internal Server Error",
-        messages: ["Internal Server Error"],
-      });
     }
   };
 
