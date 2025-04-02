eb8090daa29c794c14d201086ef55319c300757f
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2025-04-02 10:37:43 +0330

    feat: add count to getAll for pagination

--- Server_Fastify/src/modules/ContactUs/controller.ts
+++ Server_Fastify/src/modules/ContactUs/controller.ts
@@ -5,12 +5,16 @@ import { createSuccessResponse } from "../../utils/helperFuncs/helperFuncs";
 
 const controller = {
   async getAll(_: FastifyRequest, res: FastifyReply) {
+    const count: number = await contactService.getAllCount();
     const contacts = await contactService.getAll();
 
     return createSuccessResponse(res, {
       statusCode: 200,
       message: null,
-      data: contacts,
+      data: {
+        contacts,
+        count,
+      },
     });
   },
 
