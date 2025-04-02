e7330f4be7df5785ac31a492a95ea19772bcf751
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2025-04-02 15:04:06 +0330

    refactor: remove interface

--- Client_React/src/components/MenuCards/menuCards.types.ts
+++
@@ -1,6 +0,0 @@
-import { FoodType } from "../../entities/food.entity";
-import { GetAllResponse } from "../../types/pagination.type";
-
-export interface GetAllFoodsResponse extends GetAllResponse {
-  foods: FoodType[];
-}
