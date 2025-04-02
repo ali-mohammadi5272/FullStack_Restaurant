ed1d29b17068cbc884b742174220a42daaa3318f
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2025-04-02 15:06:47 +0330

    refactor: remove interface

--- Client_React/src/components/ChefCards/chefCards.type.ts
+++
@@ -1,6 +0,0 @@
-import { EmployeeType } from "../../entities/employee.entity";
-import { GetAllResponse } from "../../types/pagination.type";
-
-export interface GetAllEmployeesResponse extends GetAllResponse {
-  employees: EmployeeType[];
-}
