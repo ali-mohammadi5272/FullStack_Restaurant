b56ba455833ca90c32e6400456bc07b795960dd4
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2025-04-02 13:16:00 +0330

    feat: get all Foods with pagination

--- Client_React/src/components/MenuCards/MenuCards.tsx
+++ Client_React/src/components/MenuCards/MenuCards.tsx
@@ -7,21 +7,36 @@ import { GetAllFoodsResponse } from "./menuCards.types";
 
 const MenuCards = (): React.ReactNode => {
   const [foods, setFoods] = useState<FoodType[]>([]);
+  const [allFoodsCount, setAllFoodsCount] = useState<number>(0);
+  const [pageSize, setPageSize] = useState<number>(4);
+  const [page, setPage] = useState<number>(1);
 
   const getFoods = async (): Promise<void> => {
     const response = await request.GET<GetAllFoodsResponse>({
       url: "/foods",
       cache: {
-        key: "foods-component-unique-key",
+        key: `foods-component-page${page}-limit${pageSize}-unique-key`,
+      },
+      configs: {
+        params: {
+          limit: pageSize,
+          page,
+        },
       },
     });
 
     setFoods(response.data.data.foods);
+    setAllFoodsCount(response.data.data.count);
+  };
+
+  const paginationOnChange = (newPage: number, size: number) => {
+    setPageSize(size);
+    setPage(newPage);
   };
 
   useEffect(() => {
     getFoods();
-  }, []);
+  }, [page, pageSize]);
   return (
     <div>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5">
@@ -30,7 +45,14 @@ const MenuCards = (): React.ReactNode => {
         ))}
       </div>
       <div className="mt-20">
-        <PaginationComponent onChange={() => {}} />
+        <PaginationComponent
+          onChange={paginationOnChange}
+          total={allFoodsCount}
+          defaultPageSize={pageSize}
+          defaultCurrent={page}
+          pageSizeOptions={[4, 8, 12, 16, 20]}
+          disabled={!allFoodsCount}
+        />
       </div>
     </div>
   );
