53e343a247aa526c17d9e7a07682cd53e319f35f
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2025-04-02 15:57:09 +0330

    refactor: remove Categories from here

--- Client_React/src/pages/Menu/Menu.tsx
+++ Client_React/src/pages/Menu/Menu.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Categories from "../../components/Categories/Categories.tsx";
 import MenuCards from "../../components/MenuCards/MenuCards.tsx";
 import InnerContainer from "../../components/InnerContainer/InnerContainer.tsx";
 
@@ -9,7 +8,6 @@ const Menu = (): React.ReactNode => {
       <h2 className="block font-bold text-3xl sm:text-[45px] md:text-[60px] lg:text-[80px] text-center sm:mb-32">
         Menu
       </h2>
-      <Categories />
       <MenuCards />
     </InnerContainer>
   );
