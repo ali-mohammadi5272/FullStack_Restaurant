15fb6681d996cc228659bbae214cf670108eba31
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2025-04-02 14:30:33 +0330

    feat: can get Foods by categoryId

--- Client_React/src/components/MenuCards/MenuCards.tsx
+++ Client_React/src/components/MenuCards/MenuCards.tsx
@@ -1,12 +1,21 @@
 import React, { memo, useEffect, useState } from "react";
 import Card from "../MenuCard/MenuCard";
 import PaginationComponent from "../Pagination/Pagination";
+import Categories from "../Categories/Categories";
 import { FoodType } from "../../entities/food.entity";
 import { request } from "../../services/axios/axios";
 import { GetAllFoodsResponse } from "./menuCards.types";
+import { CategoryType } from "../../entities/category.entity";
+import { Empty } from "antd";
 
 const MenuCards = (): React.ReactNode => {
   const [foods, setFoods] = useState<FoodType[]>([]);
+  const [selectedCategory, setSelectedCategory] = useState<CategoryType>({
+    id: 0.12345,
+    title: "All",
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  });
   const [allFoodsCount, setAllFoodsCount] = useState<number>(0);
   const [pageSize, setPageSize] = useState<number>(4);
   const [page, setPage] = useState<number>(1);
@@ -15,12 +24,14 @@ const MenuCards = (): React.ReactNode => {
     const response = await request.GET<GetAllFoodsResponse>({
       url: "/foods",
       cache: {
-        key: `foods-component-page${page}-limit${pageSize}-unique-key`,
+        key: `foods-component-page${page}-limit${pageSize}-category${selectedCategory.title}-unique-key`,
       },
       configs: {
         params: {
           limit: pageSize,
           page,
+          categoryId:
+            selectedCategory.title === "All" ? null : selectedCategory.id,
         },
       },
     });
@@ -36,24 +47,32 @@ const MenuCards = (): React.ReactNode => {
 
   useEffect(() => {
     getFoods();
-  }, [page, pageSize]);
+  }, [page, pageSize, selectedCategory]);
+
   return (
     <div>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5">
-        {foods.map((card) => (
-          <Card key={card.id} src={card.image} rate={5} {...card} />
-        ))}
-      </div>
-      <div className="mt-20">
-        <PaginationComponent
-          onChange={paginationOnChange}
-          total={allFoodsCount}
-          defaultPageSize={pageSize}
-          defaultCurrent={page}
-          pageSizeOptions={[4, 8, 12, 16, 20]}
-          disabled={!allFoodsCount}
-        />
-      </div>
+      <Categories setSelectedCategory={setSelectedCategory} />
+      {foods.length > 0 ? (
+        <>
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5">
+            {foods.map((card) => (
+              <Card key={card.id} src={card.image} rate={5} {...card} />
+            ))}
+          </div>
+          <div className="mt-20">
+            <PaginationComponent
+              onChange={paginationOnChange}
+              total={allFoodsCount}
+              defaultPageSize={pageSize}
+              defaultCurrent={page}
+              pageSizeOptions={[4, 8, 12, 16, 20]}
+              disabled={!allFoodsCount}
+            />
+          </div>
+        </>
+      ) : (
+        <Empty />
+      )}
     </div>
   );
 };
