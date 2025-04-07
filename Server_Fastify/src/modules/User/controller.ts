2b8f484b05027bf1743667ed3963a2ffbfcfeb54
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2025-04-07 10:47:49 +0330

    feat: create getAll function

--- Server_Fastify/src/modules/User/controller.ts
+++ Server_Fastify/src/modules/User/controller.ts
@@ -1,3 +1,22 @@
-const controller = {};
+import service from "./service";
+import { FastifyReply } from "fastify";
+import { AuthenticatedRequest } from "../../types/AuthenticatedRequest.type";
+import { createSuccessResponse } from "../../utils/helperFuncs/helperFuncs";
+import { GetAllUsersQueryStringDto } from "./dto/get-all.dto";
+
+const controller = {
+  async getAll(
+    req: AuthenticatedRequest<{ Querystring: GetAllUsersQueryStringDto }>,
+    res: FastifyReply
+  ) {
+    const users = await service.getAll(req.query);
+
+    return createSuccessResponse(res, {
+      statusCode: 200,
+      message: null,
+      data: users,
+    });
+  },
+};
 
 export default controller;
