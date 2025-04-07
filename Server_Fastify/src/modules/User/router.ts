fc6871fa33e74678edac50ae06a73143a7479e92
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2025-04-07 10:49:01 +0330

    feat: add getAll to routes

--- Server_Fastify/src/modules/User/router.ts
+++ Server_Fastify/src/modules/User/router.ts
@@ -1,3 +1,18 @@
-const route = () => {};
+import userController from "./controller";
+import { FastifyInstance } from "fastify";
+import { GetAllUsersQueryStringDto } from "./dto/get-all.dto";
+import { auth } from "../../utils/middlewares/auth";
+import { roleAccess } from "../../utils/middlewares/roleAccess";
+import { Roles } from "./enum/roles.enum";
 
-export default route;
+const router = (server: FastifyInstance) => {
+  server.get<{ Querystring: GetAllUsersQueryStringDto }>(
+    "/",
+    {
+      preHandler: [auth, roleAccess([Roles.ADMIN])],
+    },
+    userController.getAll
+  );
+};
+
+export default router;
