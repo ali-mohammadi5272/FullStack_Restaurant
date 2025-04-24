cf50cd3a448b540f066db89c10f128283d96939d
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2025-04-24 14:46:28 +0330

    feat: create getNewAccessTokenByRefreshToken controller

--- Server_Fastify/src/modules/RefreshToken/controller.ts
+++ Server_Fastify/src/modules/RefreshToken/controller.ts
@@ -1,3 +1,25 @@
-const controller = {};
+import { AuthenticatedRequest } from "../../types/AuthenticatedRequest.type";
+import { createSuccessResponse } from "../../utils/helperFuncs/helperFuncs";
+import refreshTokenService from "./service";
+import { FastifyReply } from "fastify";
+
+const controller = {
+  async getNewAccessTokenByRefreshToken(
+    req: AuthenticatedRequest,
+    res: FastifyReply
+  ) {
+    const accessToken =
+      await refreshTokenService.createNewAccessTokenByRefreshToken(
+        req.user!.id,
+        req.refreshToken!
+      );
+
+    return createSuccessResponse(res, {
+      statusCode: 201,
+      message: null,
+      data: accessToken,
+    });
+  },
+};
 
 export default controller;
