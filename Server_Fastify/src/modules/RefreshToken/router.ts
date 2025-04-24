5399fa4bc20b084131c254f780744c21a8dfbd43
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2025-04-24 14:48:18 +0330

    feat: add getNewAccessTokenByRefreshToken to router

--- Server_Fastify/src/modules/RefreshToken/router.ts
+++ Server_Fastify/src/modules/RefreshToken/router.ts
@@ -1,5 +1,13 @@
+import refreshTokenController from "./controller";
 import { FastifyInstance } from "fastify";
+import { authRefreshToken } from "../../utils/middlewares/authRefreshToken";
 
-const router = (server: FastifyInstance) => {};
+const router = (server: FastifyInstance) => {
+  server.get(
+    "/getNewAccessToken",
+    { preHandler: [authRefreshToken] },
+    refreshTokenController.getNewAccessTokenByRefreshToken
+  );
+};
 
 export default router;
