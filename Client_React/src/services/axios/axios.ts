23c7b1a66a5b5d19dc0c6d9137a02547e3f827a1
Ali Mohammadi <ali.mohammadi5272@gmail.com>
2025-04-25 12:35:40 +0330

    refactor: separate axiosInstance and axiosInstanceWithHeader from axios.ts

--- Client_React/src/services/axios/axios.ts
+++ Client_React/src/services/axios/axios.ts
@@ -1,70 +1,13 @@
-import axios, { AxiosInstance } from "axios";
-import { CookieEnum } from "../../utils/helperFuncs/helperFuncs.type.ts";
-import { toast } from "react-toastify";
 import { RequestsObject } from "./axios.type.ts";
-import {
-  createServices,
-  getCookie,
-} from "../../utils/helperFuncs/helperFuncs.ts";
+import { createServices } from "../../utils/helperFuncs/helperFuncs.ts";
+import { axiosInstance } from "./instances/axiosInstance.ts";
+import { axiosInstanceWithHeader } from "./instances/axiosInstanceWithHeader.ts";
 
 sessionStorage.clear();
-
-const axiosInstance: AxiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_BASE_URL_API,
-});
-
-const axiosInstanceWithHeader: AxiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_BASE_URL_API,
-  headers: {
-    Authorization: `Bearer ${getCookie(CookieEnum.ACCESS_TOKEN)}`,
-  },
-});
-
-axiosInstance.interceptors.request.use(
-  (config) => {
-    return config;
-  },
-  (err) => {
-    toast.error(err.response.data.message);
-    return Promise.reject(err);
-  }
-);
-
-axiosInstance.interceptors.response.use(
-  (response) => {
-    toast.success(response.data.message);
-    return response;
-  },
-  (err) => {
-    toast.error(err.response.data.message);
-    return Promise.reject(err);
-  }
-);
-
-axiosInstanceWithHeader.interceptors.request.use(
-  (config) => {
-    return config;
-  },
-  (err) => {
-    toast.error(err.response.data.message);
-    return Promise.reject(err);
-  }
-);
-
-axiosInstanceWithHeader.interceptors.response.use(
-  (response) => {
-    toast.success(response.data.message);
-    return response;
-  },
-  (err) => {
-    toast.error(err.response.data.message);
-    return Promise.reject(err);
-  }
-);
 
 const [request, requestWithHeader]: RequestsObject[] = createServices([
   axiosInstance,
   axiosInstanceWithHeader,
 ]);
 
-export { axiosInstance, axiosInstanceWithHeader, request, requestWithHeader };
+export { request, requestWithHeader };
